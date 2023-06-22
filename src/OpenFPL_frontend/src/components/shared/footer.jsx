bc66e361319ed4e7eb886715f82514bb199db3a7
James Beadle <jamesbeadle1989@gmail.com>
2023-06-22 09:19:13 +0100

    Update branding

--- src/OpenFPL_frontend/src/components/shared/footer.jsx
+++ src/OpenFPL_frontend/src/components/shared/footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import TwitterIcon from '../../../assets/twitter.png';
+import DiscordIcon from '../../../assets/discord.png';
+import OpenChatIcon from '../../../assets/openchat.png';
 
 const MyFooter = () => {
   return (
@@ -8,6 +11,21 @@ const MyFooter = () => {
       <Container>
         <Row>
           <Col className="text-center">
+            {/* Add the icons with their respective links */}
+            <a href="https://twitter.com/ofpl_dao" target="_blank" rel="noopener noreferrer">
+              <img src={TwitterIcon} alt="Twitter" className="social-icon" />
+            </a>
+            <a href="https://discord.gg/EzqH6qYa" target="_blank" rel="noopener noreferrer">
+              <img src={DiscordIcon} alt="Discord" className="social-icon" />
+            </a>
+            <a href="https://oc.app/mww5i-uqaaa-aaaar-aqi2a-cai/?ref=zv6hh-xaaaa-aaaar-ac35q-cai" target="_blank" rel="noopener noreferrer">
+              <img src={OpenChatIcon} alt="OpenChat" className="social-icon" />
+            </a>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col className="text-center">
+            OpenFPL
           </Col>
         </Row>
       </Container>
--- src/OpenFPL_frontend/src/components/shared/navbar.jsx
+++ src/OpenFPL_frontend/src/components/shared/navbar.jsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { Link, useNavigate } from "react-router-dom";
+import LogoImage from "../../../assets/logo.png";
 
 const MyNavbar = () => {
   const { isAuthenticated } = useContext(AuthContext);
@@ -19,7 +20,10 @@ const MyNavbar = () => {
   return (
     <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="/">OpenFPL</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>
+          <img src={LogoImage} alt="footballgod" style={{ maxWidth: '200px', maxHeight: '100%' }} />
+        </Navbar.Brand>
+    
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse  id="responsive-navbar-nav" className="justify-content-end">
           <Nav.Link as={Link} to="/whitepaper" onClick={() => setExpanded(false)} className="nav-link mx-3">
