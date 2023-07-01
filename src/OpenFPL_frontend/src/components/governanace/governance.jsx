9c8e394adffa60694a8a40c035ba284211df6fef
James Beadle <jamesbeadle1989@gmail.com>
2023-07-01 04:30:26 +0100

    Update valudations UI

--- src/OpenFPL_frontend/src/components/governanace/governance.jsx
+++ src/OpenFPL_frontend/src/components/governanace/governance.jsx
@@ -8,7 +8,7 @@ import AdvertisingProposals from './advertising-proposals';
 const Governance = () => {
   
   const [isLoading, setIsLoading] = useState(true);
-  const [key, setKey] = useState('proposals');
+  const [key, setKey] = useState('valuations');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,12 +33,12 @@ const Governance = () => {
         <h1>OpenFPL DAO Governance</h1>
         <br />
         <Tabs defaultActiveKey="proposals" id="profile-tabs" className="mt-4" activeKey={key} onSelect={(k) => setKey(k)}>
-          <Tab eventKey="proposals" title="Proposals">
-              <Proposals isActive={key === 'proposals'} />
-          </Tab>  
           <Tab eventKey="valuations" title="Player Valuations">
             <PlayerValuations isActive={key === 'valuations'} />
           </Tab>
+          <Tab eventKey="proposals" title="Proposals">
+              <Proposals isActive={key === 'proposals'} />
+          </Tab>  
           <Tab eventKey="games" title="Validating Game Data">
             <ValidateGameData isActive={key === 'games'} />
           </Tab>
--- src/OpenFPL_frontend/src/components/governanace/player-valuations.jsx
+++ src/OpenFPL_frontend/src/components/governanace/player-valuations.jsx
@@ -22,12 +22,23 @@ const PlayerValuations = ({ isActive }) => {
   const [showConfirmModal, setShowConfirmModal] = useState(false);
   const [selectedVote, setSelectedVote] = useState(null);
   const [selectedPlayer, setSelectedPlayer] = useState(null);
+  const [teamColours, setTeamColours] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       const teamsData = await open_fpl_backend.getTeams();
       setTeamsData(teamsData);
+
+      const teamColors = {};
+        teamsData.forEach(team => {
+        teamColors[team.id] = {
+          primary: team.primaryColourHex,
+          secondary: team.secondaryColourHex
+        };
+      });
+      setTeamColours(teamColors);
+
       setIsLoading(false);
     };
 
@@ -132,9 +143,10 @@ const PlayerValuations = ({ isActive }) => {
     <Card className="custom-card mt-1">
       <Card.Body>
         <h2>Player Valuations</h2>
-        <p>You can rate up to 20 players per gameweek. Your votes are added to the total for each possible change and the DAO will update the players value at the end of the gameweek.</p>
+        <p>You can rate up to 20 players per gameweek. Your votes are added to the total for each possible change and the DAO will update the players value 
+          at the end of the gameweek. You can only vote for a player once per season.</p>
 
-        <p>You have {remainingVotes}/20 votes left for this week.</p>
+        <p>You have {remainingVotes}/20 players you can vote for this week.</p>
 
         {/* Add the modal */}
         <VoteConfirmationModal />
@@ -174,38 +186,75 @@ const PlayerValuations = ({ isActive }) => {
               <th>Position</th>
               <th>Name</th>
               <th>Value</th>
-              <th></th>
-              <th></th>
+              <th>Voting</th>
             </tr>
           </thead>
           <tbody>
             {players && players.map(player => (
               <tr key={player.id}>
-                <td>{(player.shirtNumber == 0) ? '-' : player.shirtNumber}</td>
-                <td>{POSITION_LABELS[player.position]}</td>
-                <td className='text-left'>
-                  
-                  <p className='mb-0'>{getFlag(player.nationality)} {player.lastName}</p>
-                  <small className='text-muted'>{player.firstName}</small><br />
-                  <small className='text-muted'>Age: {getAgeFromDOB(Number(player.dateOfBirth))}</small>
+                <td className='align-middle'>
+                  <div className='d-flex flex-column justify-content-center'>
+                    <span>{(player.shirtNumber == 0) ? '-' : player.shirtNumber}</span>
+                  </div>
+                </td>
+                <td className='align-middle'>
+                  <div className='d-flex flex-column justify-content-center'>
+                    <span>{POSITION_LABELS[player.position]}</span>
+                    <small className='text-muted'>Age: {getAgeFromDOB(Number(player.dateOfBirth))}</small>
+                  </div>
+                </td>
+                <td className='text-left align-middle'>
+                  <div className='d-flex flex-column justify-content-center'>
+                    <div style={{
+                      display: 'inline-block',
+                      paddingBottom: '2px'
+                    }}>
+                      <span className='mb-0'>
+                        {getFlag(player.nationality)} {player.lastName}<br />
+                        <small className='text-muted'>{player.firstName}</small>
+                      </span>
+                      <div style={{
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  paddingBottom: '2px'
+}}>
+  <div style={{
+    borderBottom: `3px solid ${(teamColours[player.teamId] || {}).secondary || 'defaultColor'}`,
+    width: '60%',
+  }}></div>
+  <span style={{ width: '40%', textAlign: 'right' }}>
+    {teams.find(team => team.id === player.teamId)?.friendlyName}
+  </span>
+</div>
+                      <div style={{
+                        borderBottom: `3px solid ${(teamColours[player.teamId] || {}).primary || 'defaultColor'}`,
+                        width: '100%',
+                        paddingBottom: '2px'
+                      }}></div>
+
+                    </div>
+                  </div>
                 </td>
                 <td className='align-middle'>
                   <h5 className='ml-4'>£{parseFloat(player.value).toFixed(2)}m</h5>
                 </td>
                 <td>
-                  <Button variant="danger" onClick={() => handleVote("negative", player)}>-</Button>
-                  <p>{player.negativeVotes}%</p>
-                  <p>{player.totalVotes}</p>
-                </td>
-                <td>
-                  <Button variant="warning" onClick={() => handleVote("neutral", player)}>0</Button>
-                  <p>{player.neutralVotes}%</p>
-                  <p>{player.totalVotes}</p>
-                </td>
-                <td>
-                  <Button variant="success" onClick={() => handleVote("positive", player)}>+</Button>
-                  <p>{player.positiveVotes}%</p>
-                  <p>{player.totalVotes}</p>
+                  <Row className="no-gutters">
+                    <Col>
+                      <Button variant="danger" style={{width: '100%'}} onClick={() => handleVote("negative", player)}>Decrease 0.25m</Button>
+                      <p className="text-center mt-1">50%</p>
+                    </Col>
+                    <Col>
+                      <Button variant="success" style={{width: '100%'}} onClick={() => handleVote("positive", player)}>Increase 0.25m</Button>
+                      <p className="text-center mt-1">50%</p>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <p className="text-center"><small>Total Weekly Votes: 0</small></p>
+                    </Col>
+                  </Row>
                 </td>
               </tr>
             ))}
