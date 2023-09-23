65ff1f393db212e00b76fa4903267e445269ca96
James Beadle <jamesbeadle1989@gmail.com>
2023-09-23 12:53:52 +0100

    Update add initial fixtures to use csv

--- src/OpenFPL_frontend/src/components/governanace/proposals/fixtures/add-initial-fixtures.jsx
+++ src/OpenFPL_frontend/src/components/governanace/proposals/fixtures/add-initial-fixtures.jsx
@@ -1,66 +1,46 @@
-import React, { useState, useContext } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
 import Papa from 'papaparse';
-import { OpenFPL_backend as open_fpl_backend } from '../../../../../../declarations/OpenFPL_backend';
-import { Actor } from "@dfinity/agent";
-import { AuthContext } from "../../../../contexts/AuthContext";
 
-const AddInitialFixturesProposal = () => {
-    const { authClient } = useContext(AuthContext);
+const AddInitialFixturesProposal = ({ sendDataToParent }) => {
+    const [season, setSeason] = useState("");
+    const [fixturesCount, setFixturesCount] = useState(null);
+    const [seasonFixtures, setSeasonFixtures] = useState([]);
 
-    const [csvFile, setCsvFile] = useState(null);
-    const [parsedData, setParsedData] = useState([]);
-    const [submitStatus, setSubmitStatus] = useState(null);
-
-    const handleFileChange = (e) => {
-        const file = e.target.files[0];
-        setCsvFile(file);
-
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
         if (file) {
-            Papa.parse(file, {
-                complete: (result) => {
-                    setParsedData(result.data);
-                },
-                header: true
-            });
+            const reader = new FileReader();
+            reader.readAsText(file);
+            reader.onload = () => {
+                const parsed = Papa.parse(reader.result, { header: true });
+                setSeasonFixtures(parsed.data);
+                setFixturesCount(parsed.data.length);
+            };
         }
-    }
+    };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        if (!csvFile || parsedData.length === 0) {
-            setSubmitStatus("Please upload a valid CSV file.");
-            return;
-        }
-
-        try {
-            const identity = authClient.getIdentity();
-            Actor.agentOf(open_fpl_backend).replaceIdentity(identity);
-
-            // Call your backend function here to send the parsedData
-            await open_fpl_backend.addInitialFixtures(parsedData);
-
-            setSubmitStatus("Fixtures successfully added!");
-        } catch (error) {
-            console.error("Error while adding fixtures: ", error);
-            setSubmitStatus("Failed to add fixtures. Please try again.");
-        }
-    }
+    useEffect(() => {
+        sendDataToParent({
+            season,
+            seasonFixtures
+        });
+    }, [season, seasonFixtures]);
 
     return (
-        <Form onSubmit={handleSubmit}>
+        <div>
             <Form.Group className="mb-3">
-                <Form.Label>Upload CSV File</Form.Label>
+                <Form.Label>Season</Form.Label>
+                <Form.Control type="text" value={season} onChange={e => setSeason(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Upload CSV for Fixtures</Form.Label>
                 <Form.Control type="file" accept=".csv" onChange={handleFileChange} />
             </Form.Group>
 
-            {submitStatus && <Alert variant={submitStatus.includes("success") ? "success" : "danger"} className="mt-2">{submitStatus}</Alert>}
-
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+            {fixturesCount && <p>Found {fixturesCount} fixtures.</p>}
+        </div>
     );
 };
 
