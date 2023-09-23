7cbdf4fad8834395fc201efa329a2990733e773d
James Beadle <jamesbeadle1989@gmail.com>
2023-09-23 17:19:07 +0100

    Add in fixture validation tab

--- src/OpenFPL_frontend/src/components/governanace/governance.jsx
+++ src/OpenFPL_frontend/src/components/governanace/governance.jsx
@@ -1,48 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Spinner, CardDeck, Card, Button, Modal, Dropdown, Pagination } from 'react-bootstrap';
-import { initSnsWrapper } from '@dfinity/sns';
+import React, { useState, useEffect, useContext } from 'react';
+import { Container, Spinner, CardDeck, Card, Button, Modal, Dropdown, Pagination, Tabs, Tab } from 'react-bootstrap';
+import { initSnsWrapper, SnsVote, SnsGovernanceCanister } from '@dfinity/sns';
 import { AuthContext } from "../../contexts/AuthContext";
 import AddProposalModal from './add-proposal-modal';
 
 const Governance = () => {
   const { authClient } = useContext(AuthContext);
   const [isLoading, setIsLoading] = useState(true);
-  const [proposals, setProposals] = useState([]);
   const [activeModalProposal, setActiveModalProposal] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [showAddProposalModal, setShowAddProposalModal] = useState(null);
-  const [pageNum, setPageNum] = useState(1);
   const [lastProposalId, setLastProposalId] = useState(null);
+  const [activeTab, setActiveTab] = useState("Proposals");
+  const [proposalPageNum, setProposalPageNum] = useState(1);
+  const [fixtureValidationPageNum, setFixtureValidationPageNum] = useState(1);
+  const [proposalData, setProposalData] = useState([]);
+  const [fixtureValidationData, setFixtureValidationData] = useState([]);
 
   useEffect(() => {
-    fetchViewData();
-  }, [pageNum]);
-
-  const fetchViewData = async () => {
-    setIsLoading(true);
-
-    const snsWrapper = await initSnsWrapper({
-      rootOptions: {
-        canisterId: "bboqb-jiaaa-aaaal-qb6ea-cai", 
-      },
-      authClient,
-      certified: true,
-    });
-
-    const params = {
-      limit: 100, 
-      beforeProposal: lastProposalId,
-      includeStatus: [SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_OPEN]
-    };
-
-    const fetchedProposals = await snsWrapper.listProposals(params);
-
-    if (fetchedProposals.length > 0) {
-      setLastProposalId(fetchedProposals[fetchedProposals.length - 1].getId());
+    if (activeTab === "Proposals") {
+      fetchViewData("Proposals");
+    } else {
+      fetchViewData("Fixture Validation");
     }
-    setProposals(fetchedProposals);
-    setIsLoading(false);
-  };
+  }, [proposalPageNum, fixtureValidationPageNum, activeTab]);
+
 
   const handleProposalClick = (proposal) => {
     setActiveModalProposal(proposal);
@@ -83,6 +65,47 @@ const Governance = () => {
     }
   };
 
+  const fetchViewData = async (type) => {
+    setIsLoading(true);
+    const snsWrapper = await initSnsWrapper({
+      rootOptions: {
+        canisterId: "bboqb-jiaaa-aaaal-qb6ea-cai",
+      },
+      authClient,
+      certified: true,
+    });
+
+    const baseParams = {
+      limit: 100,
+      beforeProposal: lastProposalId,
+      includeStatus: [SnsProposalDecisionStatus.PROPOSAL_DECISION_STATUS_OPEN]
+    };
+
+    let params = baseParams;
+
+    if (type === "Proposals") {
+      params.excludeType = [3000];
+    } else if (type === "Fixture Validation") {
+      params.excludeType = undefined; // Ensure other types are not excluded
+      params.includeType = [3000]; // Only include Fixture Validation type
+    }
+
+    
+    const fetchedProposals = await snsWrapper.listProposals(params);
+
+    if (fetchedProposals.length > 0) {
+      setLastProposalId(fetchedProposals[fetchedProposals.length - 1].getId());
+    }
+
+    if (type === "Proposals") {
+      setProposalData(fetchedProposals);
+    } else {
+      setFixtureValidationData(fetchedProposals);
+    }
+
+    setIsLoading(false);
+  };
+
   return (
     isLoading ? (
       <div className="customOverlay d-flex flex-column align-items-center justify-content-center">
@@ -92,44 +115,81 @@ const Governance = () => {
       <Container className="flex-grow-1 my-5">
         <h1>OpenFPL DAO Governance</h1>
         <br />
-        <Button className="mb-3 float-right" variant="primary" onClick={() => setShowAddProposalModal(true)}>New Proposal</Button>
-     
-        <CardDeck>
-          {proposals.map(proposal => (
-            <Card key={proposal.id} onClick={() => handleProposalClick(proposal)}>
-              <Card.Body>
-                <Card.Title>{proposal.title}</Card.Title>
-                <Card.Text>{proposal.description.substring(0, 100)}...</Card.Text>
-                <Button variant="success" onClick={() => handleVote('yes')}>Yes</Button>
-                <Button variant="danger" onClick={() => handleVote('no')}>No</Button>
-              </Card.Body>
-            </Card>
-          ))}
-        </CardDeck>
-        
-        <Pagination className="mt-3">
-          <Pagination.Prev onClick={() => pageNum > 1 && setPageNum(pageNum - 1)} />
-          <Pagination.Item>{pageNum}</Pagination.Item>
-          <Pagination.Next onClick={() => proposals.length === 100 && setPageNum(pageNum + 1)} />
-        </Pagination>
 
-        <Modal show={showModal} onHide={() => setShowModal(false)}>
-          <Modal.Header closeButton>
-            <Modal.Title>{activeModalProposal?.title}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {activeModalProposal?.description}
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="success" onClick={() => handleVote('yes')}>Yes</Button>
-            <Button variant="danger" onClick={() => handleVote('no')}>No</Button>
-          </Modal.Footer>
-        </Modal>
+          <Tabs activeKey={activeTab} id="controlled-tab" onSelect={key => {
+              setActiveTab(key);
+              if (key === "Proposals") {
+                  setProposalPageNum(1);
+              } else {
+                  setFixtureValidationPageNum(1);
+              }
+          }}
+          > 
+            
+          <Tab eventKey="Proposals" title="Proposals">
+            <Button className="mb-3 float-right" variant="primary" onClick={() => setShowAddProposalModal(true)}>New Proposal</Button>
         
-        <AddProposalModal
-            show={showAddProposalModal}
-            onHide={handleAddProposalModalClose}
-          />
+            <CardDeck>
+              {proposalData.length == 0 && (
+                <p>No proposals to view.</p>
+              )}
+              {proposalData.map(proposal => (
+                 <Card key={proposal.id} onClick={() => handleProposalClick(proposal)}>
+                  <Card.Body>
+                    <Card.Title>{proposal.title}</Card.Title>
+                    <Card.Text>{proposal.description.substring(0, 100)}...</Card.Text>
+                  </Card.Body>
+                </Card>
+              ))}
+            </CardDeck>
+            
+            <Pagination className="mt-3">
+                <Pagination.Prev onClick={() => proposalPageNum > 1 && setProposalPageNum(proposalPageNum - 1)} />
+                <Pagination.Item>{proposalPageNum}</Pagination.Item>
+                <Pagination.Next onClick={() => proposalData.length === 100 && setProposalPageNum(proposalPageNum + 1)} />
+            </Pagination>
+            
+            <AddProposalModal show={showAddProposalModal} onHide={handleAddProposalModalClose} />
+          </Tab>
+          
+          <Tab eventKey="Fixture Validation" title="Fixture Validation">
+            <Button className="mb-3 float-right" variant="primary" onClick={() => setShowAddProposalModal(true)}>Create Fixture Proposal</Button>
+            <CardDeck>
+              {fixtureValidationData.length == 0 && (
+                <p>No proposals to view.</p>
+              )}
+            { fixtureValidationData.map(proposal => (
+                <Card key={proposal.id} onClick={() => handleProposalClick(proposal)}>
+                  <Card.Body>
+                    <Card.Title>{proposal.title}</Card.Title>
+                    <Card.Text>{proposal.description.substring(0, 100)}...</Card.Text>
+                  </Card.Body>
+                </Card>
+              ))}
+            </CardDeck>
+            <Pagination className="mt-3">
+                <Pagination.Prev onClick={() => fixtureValidationPageNum > 1 && setFixtureValidationPageNum(fixtureValidationPageNum - 1)} />
+                <Pagination.Item>{fixtureValidationPageNum}</Pagination.Item>
+                <Pagination.Next onClick={() => fixtureValidationData.length === 100 && setFixtureValidationPageNum(fixtureValidationPageNum + 1)} />
+            </Pagination>
+          
+          </Tab>
+
+          <Modal show={showModal} onHide={() => setShowModal(false)}>
+            <Modal.Header closeButton>
+              <Modal.Title>{activeModalProposal?.title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {activeModalProposal?.description}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="success" onClick={() => handleVote('yes')}>Yes</Button>
+              <Button variant="danger" onClick={() => handleVote('no')}>No</Button>
+            </Modal.Footer>
+          </Modal>
+
+        </Tabs>
+
       </Container>
     )
   );
