850e448718d152fbd99875c549ff3d7c373df4f7
James Beadle <jamesbeadle1989@gmail.com>
2023-10-02 23:53:56 +0100

    Update select player modal css

--- src/OpenFPL_frontend/src/components/gameplay/player-details-modal.jsx
+++ src/OpenFPL_frontend/src/components/gameplay/player-details-modal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Modal, Button, Table, Container, Card } from 'react-bootstrap';
 import { getTeamById } from '../helpers';
 
