da99c7df791149df185e04611e0b5edfe40363ce
James Beadle <jamesbeadle1989@gmail.com>
2023-10-12 21:18:48 +0100

    Format Player detail modal

--- src/OpenFPL_frontend/src/components/club/player-detail-modal.jsx
+++ src/OpenFPL_frontend/src/components/club/player-detail-modal.jsx
@@ -14,6 +14,39 @@ const PlayerDetailModal = ({ show, onClose, player, playerGameweek, teams, seaso
     let goalConcededCount = 0;
     let keeperSaveCount = 0;
 
+    let pointsForAppearance = 5;
+    let pointsFor3Saves = 5;
+    let pointsForPenaltySave = 20;
+    let pointsForHighestScore = 25;
+    let pointsForRedCard = -20;
+    let pointsForPenaltyMiss = -10;
+    let pointsForEach2Conceded = -15;
+    let pointsForOwnGoal = -10;
+    let pointsForYellowCard = -5;
+    let pointsForCleanSheet = 10;
+
+    var pointsForGoal = 0;
+    var pointsForAssist = 0;
+
+    switch(player.position){
+        case 0:
+            pointsForGoal = 20;
+            pointsForAssist = 15;    
+            break;
+        case 1:
+            pointsForGoal = 20;
+            pointsForAssist = 15; 
+            break;
+        case 2:
+            pointsForGoal = 15;
+            pointsForAssist = 10; 
+            break;
+        case 3:
+            pointsForGoal = 10;
+            pointsForAssist = 10; 
+            break;
+    };
+
     playerGameweek.events.forEach((evt) => {
         switch (evt.eventType) {
             case 0:
@@ -43,84 +76,70 @@ const PlayerDetailModal = ({ show, onClose, player, playerGameweek, teams, seaso
                 return (<div className='player-modal-category-row row-bottom-border'>        
                     <div className='player-detail-modal-category-col'>Appearance</div>
                     <div className='player-detail-modal-quantity-col'>{`${evt.eventStartMinute}-${evt.eventEndMinute}'`}</div>
-                    <div className='player-detail-modal-points-col'>5</div>
+                    <div className='player-detail-modal-points-col'>{pointsForAppearance}</div>
                 </div>
                 );
             case 1:
                 return (<div className='player-modal-category-row row-bottom-border'>        
                     <div className='player-detail-modal-category-col'>Goal Scored</div>
                     <div className='player-detail-modal-quantity-col'>{`${evt.eventEndMinute}'`}</div>
-                    <div className='player-detail-modal-points-col'>5</div>
+                    <div className='player-detail-modal-points-col'>{pointsForGoal}</div>
                 </div>
                 );
             case 2:
                 return (<div className='player-modal-category-row row-bottom-border'>        
                     <div className='player-detail-modal-category-col'>Assist</div>
                     <div className='player-detail-modal-quantity-col'>{`${evt.eventEndMinute}'`}</div>
-                    <div className='player-detail-modal-points-col'>5</div>
-                </div>
-                );
-            case 3:
-                return (<div className='player-modal-category-row row-bottom-border'>        
-                    <div className='player-detail-modal-category-col'>Goal Conceded</div>
-                    <div className='player-detail-modal-quantity-col'>{`${evt.eventEndMinute}'`}</div>
-                    <div className='player-detail-modal-points-col'>-</div>
-                </div>
-                );
-            case 4:
-                return (<div className='player-modal-category-row row-bottom-border'>        
-                    <div className='player-detail-modal-category-col'>Keeper save</div>
-                    <div className='player-detail-modal-quantity-col'>-</div>
-                    <div className='player-detail-modal-points-col'>-</div>
+                    <div className='player-detail-modal-points-col'>{pointsForAssist}</div>
                 </div>
                 );
             case 5:
                 return (<div className='player-modal-category-row row-bottom-border'>        
                     <div className='player-detail-modal-category-col'>Clean Sheet</div>
                     <div className='player-detail-modal-quantity-col'>-</div>
-                    <div className='player-detail-modal-points-col'>10</div>
+                    <div className='player-detail-modal-points-col'>{pointsForCleanSheet}</div>
                 </div>
                 );
             case 6:
                 return (<div className='player-modal-category-row row-bottom-border'>        
                     <div className='player-detail-modal-category-col'>Penalty Save</div>
                     <div className='player-detail-modal-quantity-col'>-</div>
-                    <div className='player-detail-modal-points-col'>20</div>
+                    <div className='player-detail-modal-points-col'>{pointsForPenaltySave}</div>
                 </div>
                 );
             case 7:
                 return (<div className='player-modal-category-row row-bottom-border'>        
                     <div className='player-detail-modal-category-col'>Penalty Missed</div>
                     <div className='player-detail-modal-quantity-col'>-</div>
-                    <div className='player-detail-modal-points-col'>-15</div>
+                    <div className='player-detail-modal-points-col'>{pointsForPenaltyMiss}</div>
                 </div>
                 );
             case 8:
                 return (<div className='player-modal-category-row row-bottom-border'>        
                     <div className='player-detail-modal-category-col'>Yellow Card</div>
                     <div className='player-detail-modal-quantity-col'>-</div>
-                    <div className='player-detail-modal-points-col'>-5</div>
+                    <div className='player-detail-modal-points-col'>{pointsForYellowCard}</div>
                 </div>
                 );
             case 9:
                 return (<div className='player-modal-category-row row-bottom-border'>        
                     <div className='player-detail-modal-category-col'>Red Card</div>
                     <div className='player-detail-modal-quantity-col'>-</div>
-                    <div className='player-detail-modal-points-col'>-20</div>
+                    <div className='player-detail-modal-points-col'>{pointsForRedCard}</div>
                 </div>
                 );
             case 10:
                 return (<div className='player-modal-category-row row-bottom-border'>        
                     <div className='player-detail-modal-category-col'>Own Goal</div>
                     <div className='player-detail-modal-quantity-col'>{`${evt.eventEndMinute}'`}</div>
-                    <div className='player-detail-modal-points-col'>-10</div>
+                    <div className='player-detail-modal-points-col'>{pointsForOwnGoal}</div>
                 </div>
                 );
             case 11:
                 return (<div className='player-modal-category-row row-bottom-border'>        
                     <div className='player-detail-modal-category-col'>Highest Scoring Player</div>
                     <div className='player-detail-modal-quantity-col'>-</div>
-                    <div className='player-detail-modal-points-col'>25</div>
+                    <div className='player-detail-modal-points-col'>{pointsForHighestScore}</div>
                 </div>
                 );
         }
@@ -205,7 +224,7 @@ const PlayerDetailModal = ({ show, onClose, player, playerGameweek, teams, seaso
                         <div className='player-modal-category-row row-bottom-border'>
                             <div className='player-detail-modal-category-col'>Goals Conceded x {goalConcededCount}</div>
                             <div className='player-detail-modal-quantity-col'>-</div>
-                            <div className='player-detail-modal-points-col'>{-(15 * Math.floor(goalConcededCount / 2))}</div>
+                            <div className='player-detail-modal-points-col'>{-(-pointsForEach2Conceded * Math.floor(goalConcededCount / 2))}</div>
                         </div>
                     )}
                     {keeperSaveEvents.map(renderEvent)}
@@ -213,7 +232,7 @@ const PlayerDetailModal = ({ show, onClose, player, playerGameweek, teams, seaso
                         <div className='player-modal-category-row row-bottom-border'>
                             <div className='player-detail-modal-category-col'>Keeper Save x 3</div>
                             <div className='player-detail-modal-quantity-col'>-</div>
-                            <div className='player-detail-modal-points-col'>{5 * Math.floor(keeperSaveCount / 3)}</div>
+                            <div className='player-detail-modal-points-col'>{pointsFor3Saves * Math.floor(keeperSaveCount / 3)}</div>
                         </div>
                     )}
                     {otherEvents.map(renderEvent)}
