cac3429cf7c1617b7daef1e37caaea350d10c50f
James Beadle <jamesbeadle1989@gmail.com>
2023-11-08 10:10:59 +0000

    Update setup

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,23 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
-import autoprefixer from 'autoprefixer';
-const filesPath = (path: string) => `${path}`;
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: preprocess({
-        postcss: {
-        plugins: [autoprefixer],
-        },
-    }),
-    kit: {
-        adapter: adapter(),
-        files: {
-            assets: filesPath('static'),
-            lib: filesPath('src/lib'),
-            routes: filesPath('src/routes'),
-            appTemplate: filesPath('src/app.html')
-        }
-    }
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
+  },
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,34 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import environment from 'vite-plugin-environment';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:4943',
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [
+    sveltekit(),
+    environment('all', { prefix: 'CANISTER_' }),
+    environment('all', { prefix: 'DFX_' }),
+    environment({ BACKEND_CANISTER_ID: '' }),
+  ],
 });
