a2352eab912974733bdc74b20a9bdd2b9007fa02
James Beadle <jamesbeadle1989@gmail.com>
2023-11-10 19:43:00 +0000

    Update homepage style

--- src/OpenFPL_frontend/src/routes/+page.svelte
+++ src/OpenFPL_frontend/src/routes/+page.svelte
@@ -10,38 +10,48 @@
   }
 </script>
 
+<style>
+  .bg-panel{
+    background-color: rgba(46,50,58,0.9);
+  }
+</style>
+
 <Layout>
 
-  <div class="bg-gray-700 p-4 m-1">
-    <div class="flex justify-between items-center text-white">
-      <div>
-        <p>Game week</p>
-        <p class="text-xl font-bold">12</p>
-        <p>2023/24</p>
+  <div class="p-1">
+    <div class="flex">
+      <div class="flex justify-start items-center text-white space-x-4 flex-grow m-1 bg-panel p-4 rounded-md border border-gray-500">
+        <div class="flex-grow">
+          <p>Game week</p>
+          <p class="text-xl font-bold">12</p>
+          <p>2023/24</p>
+        </div>
+        <div class="flex-grow">
+          <p>Managers</p>
+          <p class="text-xl font-bold">200</p>
+          <p>Total</p>
+        </div>
       </div>
-      <div>
-        <p>Managers</p>
-        <p class="text-xl font-bold">200</p>
-        <p>Total</p>
-      </div>
-      <div>
-        <p>Weekly Prize Pool</p>
-        <p class="text-xl font-bold">0</p>
-        <p>$FPL</p>
-      </div>
-      <div>
-        <p>Upcoming Game</p>
-        <p class="text-xl font-bold">00 : 18 : 55</p>
-        <div class="flex items-center">
-          <p>WOL</p>
-          <p class="mx-2">vs</p>
-          <p>TOT</p>
+      <div class="flex justify-start items-center text-white space-x-4 flex-grow m-1 bg-panel p-4 rounded-md border border-gray-500">
+        <div class="flex-grow">
+          <p>Weekly Prize Pool</p>
+          <p class="text-xl font-bold">0</p>
+          <p>$FPL</p>
+        </div>
+        <div class="flex-grow">
+          <p>Upcoming Game</p>
+          <p class="text-xl font-bold">00 : 18 : 55</p>
+          <div class="flex items-center">
+            <p>WOL</p>
+            <p class="mx-2">vs</p>
+            <p>TOT</p>
+          </div>
         </div>
       </div>
     </div>
   </div>
-
-  <div class="bg-gray-800 p-4 m-1">
+  
+  <div class="bg-panel p-4 m-1 bg-panel p-4 rounded-md border border-gray-500">
     <ul class="flex">
       <li class="mr-4">
         <button
