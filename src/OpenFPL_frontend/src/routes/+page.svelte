c56b545eeb763c3985041ce8820f2c7bda0caf13
James Beadle <jamesbeadle1989@gmail.com>
2023-11-11 14:34:13 +0000

    Update homepage badge position

--- src/OpenFPL_frontend/src/routes/+page.svelte
+++ src/OpenFPL_frontend/src/routes/+page.svelte
@@ -15,6 +15,23 @@
   .bg-panel{
     background-color: rgba(46,50,58,0.9);
   }
+  
+  .circle-badge-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  
+  .circle-badge-icon {
+    align-self: center;
+  }
+
+  .w-v {
+    width: 20px;
+  }
+
+
 </style>
 
 <Layout>
@@ -37,32 +54,51 @@
           <p class="text-4xl mt-2 mb-2 font-bold">0</p>
           <p class="text-gray-300 text-xs">$FPL</p>
         </div>
+      </div>
+      <div class="flex justify-start items-center text-white space-x-4 flex-grow m-1 bg-panel p-4 rounded-md border border-gray-500">
+        <div class="flex-grow">
+          <p class="text-gray-300 text-xs">Next Game:</p>
+          <div class="flex items-center mb-2 mt-2">
+            <div class="w-10 ml-4 mr-4">
+              <CircleBadgeIcon primaryColour='#000000' secondaryColour='#f3f3f3' thirdColour='#211223' />
+            </div>
+            <!-- Reserve space for the 'v' with a wrapper div -->
+            <div class="w-v ml-1 mr-1 flex justify-center"> <!-- w-v is a custom width class you will define -->
+              <p class="text-xs mt-2 mb-2 font-bold">v</p>
+            </div>
+            <div class="w-10 ml-4">
+              <CircleBadgeIcon primaryColour='#000000' secondaryColour='#f3f3f3' thirdColour='#211223' />
+            </div>
+          </div>
+          <div class="flex items-center">
+            <div class="w-10 ml-4 mr-4">
+              <p class="text-gray-300 text-xs text-center">NEW</p>
+            </div>
+            <!-- Use the same reserved space for alignment -->
+            <div class="w-v ml-1 mr-1"> <!-- Keep this empty to maintain the space -->
+            </div>
+            <div class="w-10 ml-4">
+              <p class="text-gray-300 text-xs text-center">ARS</p>
+            </div>
+          </div>
+        </div>
+                
+        
+
+
+        <div class="flex-grow">
+          <p class="text-gray-300 text-xs">Kick Off:</p>
+          <div class="flex">
+            <p class="text-4xl mt-2 mb-2 font-bold">
+              00<span class="text-gray-300 text-xs ml-1">d</span> : 18<span class="text-gray-300 text-xs ml-1">h</span> : 55<span class="text-gray-300 text-xs ml-1">m</span>
+            </p>
+          </div>
+          <p class="text-gray-300 text-xs">Saturday November 11th, 2024</p>
+        </div>
         <div class="flex-grow">
           <p class="text-gray-300 text-xs">GW 11 High Score</p>
           <p class="text-4xl mt-2 mb-2 font-bold">Santi</p>
           <p class="text-gray-300 text-xs">250 points</p>
-        </div>
-      </div>
-      <div class="flex justify-start items-center text-white space-x-4 flex-grow m-1 bg-panel p-4 rounded-md border border-gray-500">
-        <div class="flex-grow">
-          <p class="text-gray-300 text-xs">Next Game: Sat 11th Nov, 12:30pm</p>
-          <div class="flex">
-            <p class="text-4xl mt-2 mb-2 font-bold mr-2 flex items-center">
-              <CircleBadgeIcon className='h-10 mr-2' primaryColour='#000000' secondaryColour='#f3f3f3' thirdColour='#211223' />
-              NEW
-            </p>
-            <p class="text-xs mt-2 mb-2 font-bold ml-4 mr-4 flex items-center text-center">
-              v
-            </p>
-            <p class="text-4xl mt-2 mb-2 font-bold flex items-center">
-              <CircleBadgeIcon className='h-10 mr-2' primaryColour='#000000' secondaryColour='#f3f3f3' thirdColour='#211223' />
-              ARS
-            </p>
-          </div>
-          <p class="text-xs">00<span class="text-gray-300 text-xs ml-1">d</span> : 18<span class="text-gray-300 text-xs ml-1">h</span> : 55<span class="text-gray-300 text-xs ml-1">m</span></p>
-        </div>
-        <div class="flex-grow">
-          <p>test</p>
         </div>
       </div>
     </div>
