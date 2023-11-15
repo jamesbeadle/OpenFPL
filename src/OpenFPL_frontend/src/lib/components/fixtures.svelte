81d21cf73db7e09a80b8922ae434174884fa8546
James Beadle <jamesbeadle1989@gmail.com>
2023-11-15 20:08:10 +0000

    Update fixture filter control style

--- src/OpenFPL_frontend/src/lib/components/fixtures.svelte
+++ src/OpenFPL_frontend/src/lib/components/fixtures.svelte
@@ -18,13 +18,15 @@
     const seasons: string[] = ['2023/24', '2022/23', '2021/22'];
     let fixtures: FixtureWithTeams[] = [];
     let teams: Team[] = [];
-
+    let selectedGameweek = 'Gameweek 12'; // Default or fetched value
+    let selectedSeason = '2023/24'; // Default or fetched value
+    let gameweeks = Array.from({ length: 38 }, (_, i) => `Gameweek ${i + 1}`);
+ 
     const fixtureService = new FixtureService();
     const teamService = new TeamService();
   
     onMount(async () => {
         try {
-            // Assuming getFixturesData() and getTeamsData() are async functions
             const fetchedFixtures = await fixtureService.getFixturesData(localStorage.getItem('fixtures_hash') ?? '');
             const fetchedTeams = await teamService.getTeamsData(localStorage.getItem('teams_hash') ?? '');
 
@@ -36,19 +38,26 @@
             }));
         } catch (error) {
             console.error('Error fetching data:', error);
-            // Handle error appropriately
         }
     });
 
-    const changeGameweek = (delta: number): void => {
-      gameweek += delta;
+    
+    const changeGameweek = (delta: number) => {
+        gameweek = Math.max(1, Math.min(38, gameweek + delta));
+    };
+
+        
+    const changeSeason = (delta: number) => {
+        gameweek = Math.max(1, Math.min(38, gameweek + delta));
     };
   
-    const selectSeason = (selectedSeason: string): void => {
-      season = selectedSeason;
+    const selectSeason = (event: Event) => {
+        if (event.target instanceof HTMLSelectElement) {
+            season = event.target.value;
+        }
     };
+
   
-    // Function to find a team by ID
     function getTeamFromId(teamId: number): Team | undefined {
         return teams.find((team) => team.id === teamId);
     }
@@ -58,70 +67,103 @@
 <div class="container mx-auto mt-4">
       
     <div class="flex flex-col space-y-4">
-    <div class="flex items-center justify-between">
-        <button
-        class="p-2 bg-blue-500 text-white rounded"
-        on:click={() => changeGameweek(-1)}>
-        &lt; GameWeek {gameweek - 1}
-        </button>
-        <select
-        class="p-2 rounded border border-gray-200"
-        bind:value={season}>
-        {#each seasons as s}
-            <option value="{s}">{s}</option>
-        {/each}
-        </select>
-        <button
-        class="p-2 bg-blue-500 text-white rounded"
-        on:click={() => changeGameweek(1)}>
-        GameWeek {gameweek + 1} &gt;
-        </button>
-    </div>
-    
-    <div class="space-y-2">
-        {#each fixtures as { fixture, homeTeam, awayTeam }}
-        <div>
-        <div class="flex items-center justify-between border-b border-gray-700 py-2">
-            <div class="flex items-center w-1/2">
-            <div class="flex w-1/2 space-x-4 items-center ">
+        <div class="flex flex-col sm:flex-row gap-4 sm:gap-8">
+            <div class="flex items-center space-x-2">
+              <button
+                class="text-2xl rounded fpl-button px-2 py-1"
+                on:click={() => changeGameweek(-1)}
+                disabled={selectedGameweek === 'Gameweek 1'}>
+                &lt;
+              </button>
+              
+              <select
+                class="p-2 fpl-dropdown text-xl min-w-[200px] md:min-w-[unset]"
+                bind:value={selectedGameweek}>
+                {#each gameweeks as gameweek}
+                  <option value="{gameweek}">{gameweek}</option>
+                {/each}
+              </select>
+              
+              <button
+                class="text-2xl rounded fpl-button px-2 py-1"
+                on:click={() => changeGameweek(1)}
+                disabled={selectedGameweek === 'Gameweek 38'}>
+                &gt;
+              </button>
+            </div>
             
-                <div class="w-10 h-10 items-center justify-center">
-                    <BadgeIcon 
-                        primaryColour="{homeTeam ? homeTeam.primaryColourHex : ''}"
-                        secondaryColour="{homeTeam ? homeTeam.secondaryColourHex : ''}"
-                        thirdColour="{homeTeam ? homeTeam.thirdColourHex : ''}"
-                    />
-                </div>
-                <span class="font-bold text-lg">v</span>
-                <div class="w-10 h-10 items-center justify-center">
-                    <BadgeIcon 
-                        primaryColour="{awayTeam ? awayTeam.primaryColourHex : ''}"
-                        secondaryColour="{awayTeam ? awayTeam.secondaryColourHex : ''}"
-                        thirdColour="{awayTeam ? awayTeam.thirdColourHex : ''}"
-                    />
-                </div>
+            <!-- Adding a separate div for the season selector -->
+            <div class="flex items-center space-x-2">
+              <button
+                class="text-2xl rounded fpl-button px-2 py-1"
+                on:click={() => changeSeason(-1)}
+                disabled={selectedSeason === seasons[0]}>
+                &lt;
+              </button>
+              
+              <select
+                class="p-2 fpl-dropdown text-xl min-w-[200px] md:min-w-[unset]"
+                bind:value={selectedSeason}>
+                {#each seasons as season}
+                  <option value="{season}">{season}</option>
+                {/each}
+              </select>
+              
+              <button
+                class="text-2xl rounded fpl-button px-2 py-1"
+                on:click={() => changeSeason(1)}
+                disabled={selectedSeason === seasons[seasons.length - 1]}>
+                &gt;
+              </button>
             </div>
-            <div class="flex w-1/2">
-                <span class="font-medium text-sm ml-4">9:30AM</span>
-            
-            </div>
-
-            </div>
-            <div class="flex items-center space-x-10 w-1/2">
-            <div class="flex flex-col">
-                <span>Crystal Palace</span>
-                <span>Tottenham Hotspur</span>
-            </div>
-            <div class="flex flex-col items-center">
-                <span>1</span>
-                <span>2</span>
-            </div>
-            </div>
-        </div>
-        </div>
+          </div>
+          
         
-        {/each}
-    </div>
+    
+        <div class="space-y-2">
+            {#each fixtures as { fixture, homeTeam, awayTeam }}
+            <div>
+            <div class="flex items-center justify-between border-b border-gray-700 py-2">
+                <div class="flex items-center w-1/2">
+                <div class="flex w-1/2 space-x-4 items-center ">
+                
+                    <div class="w-10 h-10 items-center justify-center">
+                        <BadgeIcon 
+                            primaryColour="{homeTeam ? homeTeam.primaryColourHex : ''}"
+                            secondaryColour="{homeTeam ? homeTeam.secondaryColourHex : ''}"
+                            thirdColour="{homeTeam ? homeTeam.thirdColourHex : ''}"
+                        />
+                    </div>
+                    <span class="font-bold text-lg">v</span>
+                    <div class="w-10 h-10 items-center justify-center">
+                        <BadgeIcon 
+                            primaryColour="{awayTeam ? awayTeam.primaryColourHex : ''}"
+                            secondaryColour="{awayTeam ? awayTeam.secondaryColourHex : ''}"
+                            thirdColour="{awayTeam ? awayTeam.thirdColourHex : ''}"
+                        />
+                    </div>
+                </div>
+                <div class="flex w-1/2">
+                    <span class="font-medium text-sm ml-4">9:30AM</span>
+                
+                </div>
+
+                </div>
+                <div class="flex items-center space-x-10 w-1/2">
+                <div class="flex flex-col">
+                    <span>Crystal Palace</span>
+                    <span>Tottenham Hotspur</span>
+                </div>
+                <div class="flex flex-col items-center">
+                    <span>1</span>
+                    <span>2</span>
+                </div>
+                </div>
+            </div>
+            </div>
+            
+            {/each}
+        </div>
     </div>
       
       
