75d959cdba2dcb8c0f643966d5a4c71410efea15
James Beadle <jamesbeadle1989@gmail.com>
2023-11-19 17:03:13 +0000

    Format team fixture columns

--- src/OpenFPL_frontend/src/lib/components/team-fixtures.svelte
+++ src/OpenFPL_frontend/src/lib/components/team-fixtures.svelte
@@ -8,7 +8,7 @@
     import { SystemService } from "$lib/services/SystemService";
     import { FixtureService } from "$lib/services/FixtureService";
     import { TeamService } from "$lib/services/TeamService";
-    import { formatUnixTimeToTime } from "../../utils/Helpers";
+    import { formatUnixDateToReadable, formatUnixTimeToTime } from "../../utils/Helpers";
   
     type FixtureWithTeams = {
       fixture: Fixture;
@@ -56,17 +56,24 @@
     }
   </script>
   
-  <div class="container-fluid mt-4">
+  <div class="container-fluid">
     <div class="flex flex-col space-y-4">
       <div>
+        <div class='flex justify-between p-2 border border-gray-700 py-4 bg-light-gray px-4'>
+          <div class="flex-grow w-1/6">Gameweek</div>
+          <div class="flex-grow w-1/3 text-center">Game</div>
+          <div class="flex-grow w-1/3">Date</div>
+          <div class="flex-grow w-1/4 text-center">Time</div>
+          <div class="flex-grow w-1/3">Teams</div>
+          <div class="flex-grow w-1/4">Result</div>
+        </div>
+      
         {#each filteredFixtures as { fixture, homeTeam, awayTeam }}
-          <div class={`flex items-center justify-between py-2 border-b border-gray-700  ${
-            fixture.status === 0 ? "text-gray-400" : "text-white"
-          }`}
-          >
-            <div class="flex items-center w-1/2 ml-4">
-              <div class="flex w-1/2 space-x-4 justify-center">
-                <div class="w-10 items-center justify-center">
+          <div class={`flex items-center justify-between border-b border-gray-700 p-2 px-4 ${
+            fixture.status === 0 ? "text-gray-400" : "text-white"}`}>
+              <div class="w-1/6">{fixture.gameweek}</div>
+              <div class="w-1/3 flex justify-center">  
+                <div class="w-10 items-center justify-center mr-4">
                   <a href={`/club/${fixture.homeTeamId}`}>
                     <BadgeIcon
                       primaryColour={homeTeam
@@ -80,7 +87,7 @@
                   </a>
                 </div>
                 <span class="font-bold text-lg">v</span>
-                <div class="w-10 items-center justify-center">
+                <div class="w-10 items-center justify-center ml-4">
                   <a href={`/club/${fixture.awayTeamId}`}>
                     <BadgeIcon
                       primaryColour={awayTeam
@@ -94,26 +101,21 @@
                   </a>
                 </div>
               </div>
-              <div class="flex w-1/2 md:justify-center">
-                <span class="text-sm md:text-lg ml-4 md:ml-0 text-left"
-                  >{formatUnixTimeToTime(Number(fixture.kickOff))}</span
-                >
+              
+              <div class="w-1/3">{formatUnixDateToReadable(Number(fixture.kickOff))}</div>
+              <div class="w-1/4 text-center">{formatUnixTimeToTime(Number(fixture.kickOff))}</div>
+              <div class="w-1/3">
+                <div class='flex flex-col text-xs md:text-lg'>
+                  <a href={`/club/${fixture.homeTeamId}`}>{homeTeam ? homeTeam.friendlyName : ""}</a>
+                  <a href={`/club/${fixture.awayTeamId}`}>{awayTeam ? awayTeam.friendlyName : ""}</a>
+                </div>
               </div>
-            </div>
-            <div class="flex items-center space-x-10 w-1/2 md:justify-center">
-              <div class='flex flex-col min-w-[120px] md:min-w-[300px] text-xs md:text-lg'>
-                <a href={`/club/${fixture.homeTeamId}`}
-                  >{homeTeam ? homeTeam.friendlyName : ""}</a
-                >
-                <a href={`/club/${fixture.awayTeamId}`}
-                  >{awayTeam ? awayTeam.friendlyName : ""}</a
-                >
+              <div class="w-1/4">
+                <div class='flex flex-col text-xs md:text-lg'>
+                  <span>{fixture.status === 0 ? "-" : fixture.homeGoals}</span>
+                  <span>{fixture.status === 0 ? "-" : fixture.awayGoals}</span>
+                </div>
               </div>
-              <div class='flex flex-col min-w-[120px] md:min-w-[300px] text-xs md:text-lg'>
-                <span>{fixture.status === 0 ? "-" : fixture.homeGoals}</span>
-                <span>{fixture.status === 0 ? "-" : fixture.awayGoals}</span>
-              </div>
-            </div>
           </div>
         {/each}
       </div>
