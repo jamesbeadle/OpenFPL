6d45f1cc7b8dea66d2b5c129cf5553ea05d61bdf
James Beadle <jamesbeadle1989@gmail.com>
2023-11-19 07:23:08 +0000

    Format homepage leaderboard

--- src/OpenFPL_frontend/src/lib/components/league-table.svelte
+++ src/OpenFPL_frontend/src/lib/components/league-table.svelte
@@ -140,80 +140,77 @@
   }
 
 </script>
-
 <div class="container-fluid mt-4">
-  <div class="flex flex-col space-y-4">
-    <div class="flex flex-col sm:flex-row gap-4 sm:gap-8">
-      <div class="flex items-center space-x-2 ml-4">
-        <button
-          class="text-2xl rounded fpl-button px-2 py-1"
-          on:click={() => changeGameweek(-1)}
-          disabled={selectedGameweek === 1}
-        >
-          &lt;
-        </button>
+  <!-- Gameweek Selector -->
+  <div class="flex flex-col sm:flex-row gap-4 sm:gap-8">
+    <div class="flex items-center space-x-2 ml-4">
+      <button
+        class="text-2xl rounded fpl-button px-2 py-1"
+        on:click={() => changeGameweek(-1)}
+        disabled={selectedGameweek === 1}
+      >
+        &lt;
+      </button>
 
-        <select
-          class="p-2 fpl-dropdown text-sm md:text-xl text-center"
-          bind:value={selectedGameweek}
-        >
-          {#each gameweeks as gameweek}
-            <option value={gameweek}>Gameweek {gameweek}</option>
-          {/each}
-        </select>
+      <select
+        class="p-2 fpl-dropdown text-sm md:text-xl text-center"
+        bind:value={selectedGameweek}
+      >
+        {#each gameweeks as gameweek}
+          <option value={gameweek}>Gameweek {gameweek}</option>
+        {/each}
+      </select>
 
-        <button
-          class="text-2xl rounded fpl-button px-2 py-1"
-          on:click={() => changeGameweek(1)}
-          disabled={selectedGameweek === 38}
-        >
-          &gt;
-        </button>
-      </div>
+      <button
+        class="text-2xl rounded fpl-button px-2 py-1"
+        on:click={() => changeGameweek(1)}
+        disabled={selectedGameweek === 38}
+      >
+        &gt;
+      </button>
+    </div>
+  </div>
 
-      <div class="overflow-x-auto">
+  <div class="flex flex-col space-y-4 mt-4 text-lg">
+      <div class="overflow-x-auto flex-1">
         <!-- Table Header -->
-        <div class='flex justify-between bg-gray-200 p-2'>
-          <div class="w-12 text-center">Pos</div>
-          <div class="flex-1 text-center">Team</div>
-          <div class="w-12 text-center">P</div>
-          <div class="w-12 text-center">W</div>
-          <div class="w-12 text-center">D</div>
-          <div class="w-12 text-center">L</div>
-          <div class="w-12 text-center">GF</div>
-          <div class="w-12 text-center">GA</div>
-          <div class="w-12 text-center">GD</div>
-          <div class="w-12 text-center">PTS</div>
+        <div class='flex justify-between p-2 border border-gray-700 py-4 bg-light-gray'>
+          <div class="w-1/12 text-center mx-4">Pos</div>
+          <div class="flex-grow px-4">Team</div>
+          <div class="w-1/12 text-center">P</div>
+          <div class="w-1/12 text-center">W</div>
+          <div class="w-1/12 text-center">D</div>
+          <div class="w-1/12 text-center">L</div>
+          <div class="w-1/12 text-center">GF</div>
+          <div class="w-1/12 text-center">GA</div>
+          <div class="w-1/12 text-center">GD</div>
+          <div class="w-1/12 text-center">PTS</div>
         </div>
   
         <!-- Table Rows -->
         {#each tableData as team, idx}
-          <div class="flex justify-between items-center p-2 hover:bg-gray-100 cursor-pointer">
-              <div class="w-12 text-center">{idx + 1}</div>
-              <div class="flex-1 flex items-center justify-start space-x-2">
-                <a href={`/club?id=${team.id}`}>
-                  
-                  <BadgeIcon
-                  primaryColour={team.primaryColourHex}
-                  secondaryColour={team.secondaryColourHex}
-                  thirdColour={team.thirdColourHex}
-                  className='w-6 h-6'
-                />
-                {team.friendlyName}
-                </a>
-              </div>
-              <div class="w-12 text-center">{team.played}</div>
-              <div class="w-12 text-center">{team.wins}</div>
-              <div class="w-12 text-center">{team.draws}</div>
-              <div class="w-12 text-center">{team.losses}</div>
-              <div class="w-12 text-center">{team.goalsFor}</div>
-              <div class="w-12 text-center">{team.goalsAgainst}</div>
-              <div class="w-12 text-center">{team.goalsFor - team.goalsAgainst}</div>
-              <div class="w-12 text-center">{team.points}</div>
-              
-          </div>
-        {/each}
+  <div class="flex items-center justify-between py-4 border-b border-gray-700 cursor-pointer">
+      <div class="w-1/12 text-center mx-4">{idx + 1}</div> <!-- Adjusted width -->
+      <a class="flex-grow flex items-center justify-start space-x-2 px-4 min-w-[200px] max-w-[300px]" href={`/club?id=${team.id}`}>
+        
+        <BadgeIcon
+        primaryColour={team.primaryColourHex}
+        secondaryColour={team.secondaryColourHex}
+        thirdColour={team.thirdColourHex}
+        className='w-6 h-6 mr-2'
+      />
+      {team.friendlyName}
+      </a>
+      <div class="w-1/12 text-center">{team.played}</div>
+      <div class="w-1/12 text-center">{team.wins}</div>
+      <div class="w-1/12 text-center">{team.draws}</div>
+      <div class="w-1/12 text-center">{team.losses}</div>
+      <div class="w-1/12 text-center">{team.goalsFor}</div>
+      <div class="w-1/12 text-center">{team.goalsAgainst}</div>
+      <div class="w-1/12 text-center">{team.goalsFor - team.goalsAgainst}</div>
+      <div class="w-1/12 text-center">{team.points}</div>
+  </div>
+{/each}
       </div>
-    </div>
   </div>
 </div>
