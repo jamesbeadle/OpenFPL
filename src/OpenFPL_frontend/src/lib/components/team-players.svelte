8481b0ed41906b8d2c944727602be77399104daf
James Beadle <jamesbeadle1989@gmail.com>
2023-11-19 15:48:38 +0000

    Add country flags to player list

--- src/OpenFPL_frontend/src/lib/components/team-players.svelte
+++ src/OpenFPL_frontend/src/lib/components/team-players.svelte
@@ -1,69 +1,42 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import BadgeIcon from "$lib/icons/BadgeIcon.svelte";
-    import type { Team } from "../../../../declarations/OpenFPL_backend/OpenFPL_backend.did";
-    import { SystemService } from "$lib/services/SystemService";
-    import { TeamService } from "$lib/services/TeamService";
-    import type { PlayerDTO } from "../../../../declarations/player_canister/player_canister.did";
-    import { convertDateToReadable, getPositionText } from "../../utils/Helpers";
-    const teamService = new TeamService();
-    const systemService = new SystemService();
-
-    export let players: PlayerDTO[] = [];
+  import type { PlayerDTO } from "../../../../declarations/player_canister/player_canister.did";
+  import { convertDateToReadable, getFlagComponent, getPositionText } from "../../utils/Helpers";  
+  export let players: PlayerDTO[] = [];
+</script>
   
-    let teams: Team[] = [];
-    
-    onMount(async () => {
-      try {
-        const fetchedTeams = await teamService.getTeamsData(
-          localStorage.getItem("teams_hash") ?? ""
-        );
-  
-        teams = fetchedTeams;
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    });
-  
-    function getTeamFromId(teamId: number): Team | undefined {
-      return teams.find((team) => team.id === teamId);
-    }
-  </script>
-  
-  <div class="container-fluid mt-4">
-    <div class="flex flex-col space-y-4">
-      <div>
-        {#each players as player }
-          <div class="flex items-center justify-between py-2 border-b border-gray-700 text-white cursor-pointer">
-            <a class="flex-grow flex items-center justify-start space-x-2 px-4" href={`/player?id=${player.id}`}>
-                <div class="flex items-center w-1/2 ml-4">
-                    {player.shirtNumber == 0 ? '-' : player.shirtNumber}
-                </div>
-                <div class="flex items-center w-1/2 ml-4">
-                    {player.firstName == "" ? '-' : player.firstName}
-                </div>
-                <div class="flex items-center w-1/2 ml-4">
-                    {player.lastName}
-                </div>
-                <div class="flex items-center w-1/2 ml-4">
-                    { convertDateToReadable(Number(player.dateOfBirth))}
-                </div>
-                <div class="flex items-center w-1/2 ml-4">
-                    { player.nationality }
-                </div>
-                <div class="flex items-center w-1/2 ml-4">
-                    { player.totalPoints }
-                </div>
-                <div class="flex items-center w-1/2 ml-4">
-                    £{ (Number(player.value)/4).toFixed(2) }m
-                </div>
-                <div class="flex items-center w-1/2 ml-4">
-                    {getPositionText(player.position)}
-                </div>
-            </a>
-          </div>
-        {/each}
-      </div>
+<div class="container-fluid mt-4">
+  <div class="flex flex-col space-y-4">
+    <div>
+      {#each players as player }
+        <div class="flex items-center justify-between py-2 border-b border-gray-700 text-white cursor-pointer">
+          <a class="flex-grow flex items-center justify-start space-x-2 px-4" href={`/player?id=${player.id}`}>
+              <div class="flex items-center w-1/2 ml-4">
+                  {player.shirtNumber == 0 ? '-' : player.shirtNumber}
+              </div>
+              <div class="flex items-center w-1/2 ml-4">
+                  {player.firstName == "" ? '-' : player.firstName}
+              </div>
+              <div class="flex items-center w-1/2 ml-4">
+                  {player.lastName}
+              </div>
+              <div class="flex items-center w-1/2 ml-4">
+                  { convertDateToReadable(Number(player.dateOfBirth))}
+              </div>
+              <div class="flex items-center w-1/2 ml-4">
+                <svelte:component class="w-10 h-10" this={getFlagComponent(player.nationality)} size="100" /> 
+              </div>
+              <div class="flex items-center w-1/2 ml-4">
+                  { player.totalPoints }
+              </div>
+              <div class="flex items-center w-1/2 ml-4">
+                  £{ (Number(player.value)/4).toFixed(2) }m
+              </div>
+              <div class="flex items-center w-1/2 ml-4">
+                  {getPositionText(player.position)}
+              </div>
+          </a>
+        </div>
+      {/each}
     </div>
   </div>
-  
+</div>
--- src/OpenFPL_frontend/src/utils/Helpers.ts
+++ src/OpenFPL_frontend/src/utils/Helpers.ts
@@ -69,3 +69,141 @@ export function convertDateToReadable(nanoseconds: number): string {
   const date = new Date(milliseconds);
   return date.toLocaleDateString('en-GB');
 }
+
+import * as FlagIcons from 'svelte-flag-icons';
+export function getFlagComponent(countryCode: string) {
+  switch (countryCode) {
+    case 'Albania':
+      return FlagIcons.Al;
+    case 'Algeria':
+      return FlagIcons.Dz;
+    case 'Argentina':
+      return FlagIcons.Ar;
+    case 'Australia':
+      return FlagIcons.Au;
+    case 'Austria':
+      return FlagIcons.At;
+    case 'Belgium':
+      return FlagIcons.Be;
+    case 'Bosnia and Herzegovina':
+      return FlagIcons.Ba;
+    case 'Brazil':
+      return FlagIcons.Br;
+    case 'Burkina Faso':
+      return FlagIcons.Bf;
+    case 'Cameroon':
+      return FlagIcons.Cm;
+    case 'Canada':
+      return FlagIcons.Ca;
+    case 'Colombia':
+      return FlagIcons.Co;
+    case 'Costa Rica':
+      return FlagIcons.Cr;
+    case 'Croatia':
+      return FlagIcons.Hr;
+    case 'Czech Republic':
+      return FlagIcons.Cz;
+    case 'Denmark':
+      return FlagIcons.Dk;
+    case 'DR Congo':
+      return FlagIcons.Cg;
+    case 'Ecuador':
+      return FlagIcons.Ec;
+    case 'Egypt':
+      return FlagIcons.Eg;
+    case 'England':
+      return FlagIcons.Gb;
+    case 'Estonia':
+      return FlagIcons.Ee;
+    case 'Finland':
+      return FlagIcons.Fi;
+    case 'France':
+      return FlagIcons.Fr;
+    case 'Gabon':
+      return FlagIcons.Ga;
+    case 'Germany':
+      return FlagIcons.De;
+    case 'Ghana':
+      return FlagIcons.Gh;
+    case 'Greece':
+      return FlagIcons.Gr;
+    case 'Grenada':
+      return FlagIcons.Gd;
+    case 'Guinea':
+      return FlagIcons.Gn;
+    case 'Iceland':
+      return FlagIcons.Is;
+    case 'Iran':
+      return FlagIcons.Ir;
+    case 'Ireland':
+      return FlagIcons.Ie;
+    case 'Israel':
+      return FlagIcons.Il;
+    case 'Italy':
+      return FlagIcons.It;
+    case 'Ivory Coast':
+      return FlagIcons.Ci;
+    case 'Jamaica':
+      return FlagIcons.Jm;
+    case 'Japan':
+      return FlagIcons.Jp;
+    case 'Macedonia':
+      return FlagIcons.Mk;
+    case 'Mali':
+      return FlagIcons.Ml;
+    case 'Mexico':
+      return FlagIcons.Mx;
+    case 'Montserrat':
+      return FlagIcons.Ms;
+    case 'Morocco':
+      return FlagIcons.Ma;
+    case 'Netherlands':
+      return FlagIcons.Nl;
+    case 'Nigeria':
+      return FlagIcons.Ne;
+    case 'Northern Ireland':
+      return FlagIcons.Gb;
+    case 'Norway':
+      return FlagIcons.No;
+    case 'Paraguay':
+      return FlagIcons.Py;
+    case 'Poland':
+      return FlagIcons.Pl;
+    case 'Portugal':
+      return FlagIcons.Pt;
+    case 'Scotland':
+      return FlagIcons.Gb;
+    case 'Senegal':
+      return FlagIcons.Sn;
+    case 'Serbia':
+      return FlagIcons.Rs;
+    case 'Slovakia':
+      return FlagIcons.Sk;
+    case 'South Africa':
+      return FlagIcons.Za;
+    case 'South Korea':
+      return FlagIcons.Kr;
+    case 'Spain':
+      return FlagIcons.Es;
+    case 'Sweden':
+      return FlagIcons.Se;
+    case 'Switzerland':
+      return FlagIcons.Ch;
+    case 'Tunisia':
+      return FlagIcons.Tn;
+    case 'Turkey':
+      return FlagIcons.Tr;
+    case 'Ukraine':
+      return FlagIcons.Ua;
+    case 'United States':
+      return FlagIcons.Us;
+    case 'Uruguay':
+      return FlagIcons.Uy;
+    case 'Wales':
+      return FlagIcons.Gb;
+    case 'Zimbabwe':
+      return FlagIcons.Zw;
+    default:
+      return null;
+  }
+}
