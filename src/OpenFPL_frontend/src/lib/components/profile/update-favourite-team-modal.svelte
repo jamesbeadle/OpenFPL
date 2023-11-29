6078e5abcb6df34061c08fadd9f6c3ef72d3ffca
James Beadle <jamesbeadle1989@gmail.com>
2023-11-29 05:02:22 +0000

    Change button colour on disabled

--- src/OpenFPL_frontend/src/lib/components/profile/update-favourite-team-modal.svelte
+++ src/OpenFPL_frontend/src/lib/components/profile/update-favourite-team-modal.svelte
@@ -77,7 +77,7 @@
           Cancel
         </button>
         <button
-          class={`px-4 py-2 fpl-purple-btn text-white text-base font-medium rounded-md w-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-300`}
+          class={`px-4 py-2 ${isSubmitDisabled ? "bg-gray-500" : "fpl-purple-btn"} text-white text-base font-medium rounded-md w-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-300`}
           on:click={updateFavouriteTeam}
           disabled={isSubmitDisabled}>
           Use
