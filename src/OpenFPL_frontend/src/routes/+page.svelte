f3cbd927bf3d9912a5a7e6c1af43db08ed7b7ae4
James Beadle <jamesbeadle1989@gmail.com>
2023-12-06 13:35:08 +0000

    Update homepage tailwind

--- src/OpenFPL_frontend/src/routes/+page.svelte
+++ src/OpenFPL_frontend/src/routes/+page.svelte
@@ -131,7 +131,7 @@
             <p
               class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold"
             >
-              {managerCount * 10000000}
+              {managerCount}
             </p>
             <p class="text-gray-300 text-xxs xs:text-sm sm:text-base">Total</p>
           </div>
@@ -140,37 +140,34 @@
             style="min-width: 2px; min-height: 50px;"
           />
           <div class="flex-grow">
-            <p
-              class="text-gray-300 hidden md:block text-xxs xs:text-sm sm:text-base"
-            >
+            <p class="text-gray-300 hidden md:block text-xxs xs:text-sm sm:text-base">
               Weekly Prize Pool
             </p>
             <p class="text-gray-300 md:hidden text-xxs xs:text-sm sm:text-base">
               Weekly
             </p>
-            <p
-              class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold"
-            >
-              5000
+            <p class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold">
+              0
             </p>
             <p class="text-gray-300 text-xxs text-xxs xs:text-sm sm:text-base">
               $FPL
             </p>
           </div>
         </div>
-        <div
-          class="flex flex-col lg:flex-row justify-start lg:items-center text-white space-x-0 lg:space-x-4 flex-grow bg-panel p-4 rounded-md"
-        >
+
+
+
+
+
+
+
+        <div class="md:hidden lg:flex flex-col lg:flex-row justify-start lg:items-center text-white space-x-0 lg:space-x-4 flex-grow bg-panel p-4 rounded-md">
           <div class="flex-grow mb-3 md:mb-4 lg:mb-0">
-            <p
-              class="text-gray-300 text-xxs xs:text-sm sm:text-base w-full text-center lg:w-auto lg:text-left"
-            >
+            <p class="text-gray-300 text-xxs xs:text-sm sm:text-base w-full text-center lg:w-auto lg:text-left">
               Next Game
             </p>
             <div class="flex justify-center">
-              <div
-                class="flex justify-center items-center mb-2 text-xxs xs:text-sm sm:text-base"
-              >
+              <div class="flex justify-center items-center mb-2 text-xxs xs:text-sm sm:text-base">
                 <div class="ml-4 lg:ml-1 xl:ml-4 mr-4 lg:mr-1 xl:mr-4 flex">
                   <a
                     class="flex flex-col items-center justify-center mt-6 lg:mt-3 xl:mt-6"
@@ -197,9 +194,7 @@
                     </span>
                   </a>
                 </div>
-                <div
-                  class="w-v ml-1 mr-1 flex justify-center mt-6 lg:mt-2 xl:mt-6"
-                >
+                <div class="w-v ml-1 mr-1 flex justify-center mt-6 lg:mt-2 xl:mt-6">
                   <p class="text-xs mt-2 mb-2 font-bold">v</p>
                 </div>
                 <div class="ml-4 lg:ml-1 xl:ml-4 mr-4 lg:mr-1 xl:mr-4 flex">
@@ -231,21 +226,15 @@
               </div>
             </div>
           </div>
-          <div
-            class="h-px bg-gray-400 w-full lg:w-px lg:h-full lg:self-stretch"
-            style="min-height: 2px; min-width: 2px;"
-          />
+          
+          <div class="h-px bg-gray-400 w-full lg:w-px lg:h-full lg:self-stretch" style="min-height: 2px; min-width: 2px;" />
 
           <div class="flex-grow mb-4 lg:mb-0">
-            <p
-              class="text-gray-300 text-xxs xs:text-sm sm:text-base mt-4 lg:mt-0"
-            >
+            <p class="text-gray-300 text-xxs xs:text-sm sm:text-base mt-4 lg:mt-0">
               Kick Off
             </p>
             <div class="flex">
-              <p
-                class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold"
-              >
+              <p class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold">
                 {countdownDays}<span class="text-gray-300 text-xs ml-1">d</span>
                 : {countdownHours}<span class="text-gray-300 text-xs ml-1"
                   >h</span
@@ -255,30 +244,21 @@
                 >
               </p>
             </div>
-            <p
-              class="lg:hidden xl:flex text-gray-300 text-xxs xs:text-sm sm:text-base"
-            >
+            <p class="lg:hidden xl:flex text-gray-300 text-xxs xs:text-sm sm:text-base">
               {nextFixtureDate} | {nextFixtureTime}
             </p>
-            <p
-              class="hidden lg:flex xl:hidden text-gray-300 text-xxs xs:text-sm sm:text-base"
-            >
+            <p class="hidden lg:flex xl:hidden text-gray-300 text-xxs xs:text-sm sm:text-base">
               {nextFixtureDateSmall}
             </p>
           </div>
-          <div
-            class="h-px bg-gray-400 w-full lg:w-px lg:h-full lg:self-stretch"
-            style="min-height: 2px; min-width: 2px;"
-          />
+
+          <div class="h-px bg-gray-400 w-full lg:w-px lg:h-full lg:self-stretch" style="min-height: 2px; min-width: 2px;"/>
+
           <div class="flex-grow">
-            <p
-              class="text-gray-300 text-xxs xs:text-sm sm:text-base mt-4 lg:mt-0"
-            >
+            <p class="text-gray-300 text-xxs xs:text-sm sm:text-base mt-4 lg:mt-0">
               GW {$systemStore?.focusGameweek} High Score
             </p>
-            <p
-              class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold max-w-[200px] truncate"
-            >
+            <p class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold max-w-[200px] truncate">
               {#if weeklyLeader}
                 <a
                   href={`/manager?id=${weeklyLeader.principalId}&gw=${$systemStore?.focusGameweek}`}
@@ -299,23 +279,138 @@
             </p>
           </div>
         </div>
+
+
+
+        <div
+          class="hidden md:flex lg:hidden justify-start items-center text-white rounded-md bg-panel
+          space-x-2 sm:space-x-3 md:space-x-4 flex-grow
+          p-2 xs:p-3 sm:p-4
+          mb-5 xs:mb-7 sm:mb-8 lg:mb-0"
+        >
+          <div class="flex-grow">
+            <p class="text-gray-300 text-xxs xs:text-sm sm:text-base w-full text-center lg:w-auto lg:text-left">
+              Next Game
+            </p>
+            <div class="flex flex-row">
+              <div class="ml-4 lg:ml-1 xl:ml-4 mr-4 lg:mr-1 xl:mr-4 flex">
+                <a
+                  class="flex flex-col items-center justify-center mt-6 lg:mt-3 xl:mt-6"
+                  href={`/club?id=${
+                    nextFixtureHomeTeam ? nextFixtureHomeTeam.id : -1
+                  }`}
+                >
+                  <BadgeIcon
+                    className="h-8 lg:h-7 xl:h-8 mb-4 lg:mb-2 xl:mb-4"
+                    primaryColour={nextFixtureHomeTeam
+                      ? nextFixtureHomeTeam.primaryColourHex
+                      : ""}
+                    secondaryColour={nextFixtureHomeTeam
+                      ? nextFixtureHomeTeam.secondaryColourHex
+                      : ""}
+                    thirdColour={nextFixtureHomeTeam
+                      ? nextFixtureHomeTeam.thirdColourHex
+                      : ""}
+                  />
+                  <span>
+                    {nextFixtureHomeTeam
+                      ? nextFixtureHomeTeam.abbreviatedName
+                      : ""}
+                  </span>
+                </a>
+              </div>
+              <div class="w-v ml-1 mr-1 flex justify-center mt-6 lg:mt-2 xl:mt-6">
+                <p class="text-xs mt-2 mb-2 font-bold">v</p>
+              </div>
+              <div class="ml-4 lg:ml-1 xl:ml-4 mr-4 lg:mr-1 xl:mr-4 flex">
+                <a
+                  class="flex flex-col items-center justify-center mt-6 lg:mt-3 xl:mt-6"
+                  href={`/club?id=${
+                    nextFixtureAwayTeam ? nextFixtureAwayTeam.id : -1
+                  }`}
+                >
+                  <BadgeIcon
+                    className="h-8 lg:h-7 xl:h-8 mb-4 lg:mb-2 xl:mb-4"
+                    primaryColour={nextFixtureAwayTeam
+                      ? nextFixtureAwayTeam.primaryColourHex
+                      : ""}
+                    secondaryColour={nextFixtureAwayTeam
+                      ? nextFixtureAwayTeam.secondaryColourHex
+                      : ""}
+                    thirdColour={nextFixtureAwayTeam
+                      ? nextFixtureAwayTeam.thirdColourHex
+                      : ""}
+                  />
+                  <span>
+                    {nextFixtureAwayTeam
+                      ? nextFixtureAwayTeam.abbreviatedName
+                      : ""}
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            class="flex-shrink-0 w-px bg-gray-400 self-stretch"
+            style="min-width: 2px; min-height: 50px;"
+          />
+          <div class="flex-grow">
+            <p class="text-gray-300 text-xxs xs:text-sm sm:text-base">
+              Kick Off
+            </p>
+            <p class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold">
+              {countdownDays}<span class="text-gray-300 text-xs ml-1">d</span>
+              : {countdownHours}<span class="text-gray-300 text-xs ml-1"
+                >h</span
+              >
+              : {countdownMinutes}<span class="text-gray-300 text-xs ml-1"
+                >m</span
+              >
+            </p>
+            <p class="lg:hidden xl:flex text-gray-300 text-xxs xs:text-sm sm:text-base">
+              {nextFixtureDate} | {nextFixtureTime}
+            </p>
+          </div>
+          <div
+            class="flex-shrink-0 w-px bg-gray-400 self-stretch"
+            style="min-width: 2px; min-height: 50px;"
+          />
+          <div class="flex-grow">
+            <p class="text-gray-300 hidden md:block text-xxs xs:text-sm sm:text-base">
+              GW {$systemStore?.focusGameweek} High Score
+            </p>
+            <p class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold max-w-[200px] truncate">
+              {#if weeklyLeader}
+                <a
+                  href={`/manager?id=${weeklyLeader.principalId}&gw=${$systemStore?.focusGameweek}`}
+                  >{weeklyLeader.principalId === weeklyLeader.username
+                    ? "Unknown"
+                    : weeklyLeader.username}</a
+                >
+              {:else}
+                -
+              {/if}
+            </p>
+            <p class="text-gray-300 text-xxs text-xxs xs:text-sm sm:text-base">
+              {#if weeklyLeader}
+                {weeklyLeader.points} points
+              {:else}
+                -
+              {/if}
+            </p>
+          </div>
+        </div>
+
+        
+        
       </div>
     </div>
 
     <div class="mx-4 xs:mx-6 sm:mx-8 lg:mx-10 mb-4 xs:mb-6 sm:mb-8">
       <div class="bg-panel rounded-md">
-        <ul
-          class="flex bg-light-gray px-1 md:px-4 pt-2 text-xxs sm:text-base md:text-lg border-b border-gray-700"
-        >
-          <li
-            class={`mr-1 md:mr-4 ${
-              activeTab === "fixtures" ? "active-tab" : ""
-            }`}
-          >
-            <button
-              class={`p-2 ${
-                activeTab === "fixtures" ? "text-white" : "text-gray-400"
-              }`}
+        <ul class="flex bg-light-gray px-1 md:px-4 pt-2 text-xxs sm:text-base md:text-lg border-b border-gray-700">
+          <li class={`mr-1 md:mr-4 ${ activeTab === "fixtures" ? "active-tab" : "" }`}>
+            <button class={`p-2 ${ activeTab === "fixtures" ? "text-white" : "text-gray-400" }`}
               on:click={() => setActiveTab("fixtures")}
             >
               Fixtures
