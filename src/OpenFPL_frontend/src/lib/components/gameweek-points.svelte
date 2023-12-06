9b3474d63b99a23fb765f26e7e707c48f740ba8a
James Beadle <jamesbeadle1989@gmail.com>
2023-12-06 18:08:37 +0000

    Update gameweek points layout

--- src/OpenFPL_frontend/src/lib/components/gameweek-points.svelte
+++ src/OpenFPL_frontend/src/lib/components/gameweek-points.svelte
@@ -162,25 +162,25 @@
           </div>
         </div>
       </div>
-      <div class="flex flex-col space-y-4 mt-4 text-lg text-xs md:text-base">
+      <div class="flex flex-col space-y-4 mt-4 text-xxs xs:text-xs md:text-base">
         <div class="overflow-x-auto flex-1">
           <div
             class="flex justify-between border-b border-t border-gray-700 p-4 bg-light-gray"
           >
-            <div class="w-1/6">Pos</div>
-            <div class="w-3/6">Player</div>
-            <div class="w-1/6">Points</div>
-            <div class="w-1/6">&nbsp;</div>
+            <div class="w-2/12 xs:w-2/12">Pos</div>
+            <div class="w-6/12 xs:w-4/12">Player</div>
+            <div class="w-3/12 xs:w-3/12">Points</div>
+            <div class="w-2/12 xs:w-3/12">&nbsp;</div>
           </div>
           {#if gameweekData.length > 0}
             {#each gameweekData as playerGameweek}
               <div
                 class="flex items-center justify-between p-4 border-b border-gray-700 cursor-pointer"
               >
-                <div class="w-1/6">
+                <div class="w-2/12 xs:w-2/12">
                   {getPositionAbbreviation(playerGameweek.player.position)}
                 </div>
-                <div class="w-3/6">
+                <div class="w-6/12 xs:w-4/12">
                   <a href={`/player?id=${playerGameweek.player.id}`}>
                     {playerGameweek.player.firstName.length > 0
                       ? playerGameweek.player.firstName.substring(0, 1) + "."
@@ -188,11 +188,11 @@
                     {playerGameweek.player.lastName}</a
                   >
                 </div>
-                <div class="w-1/6">{playerGameweek.points}</div>
-                <div class="w-1/6">
+                <div class="w-3/12 xs:w-3/12">{playerGameweek.points}</div>
+                <div class="w-2/12 xs:w-3/12 flex items-center justify-center xs:justify-start">
                   <button on:click={() => showDetailModal(playerGameweek)}>
                     <span class="flex items-center">
-                      <ViewDetailsIcon className="w-6 mr-2" />View Details
+                      <ViewDetailsIcon className="w-5 xs:w-6" /><span class="hidden xs:flex ml-1">View Details</span>
                     </span>
                   </button>
                 </div>
--- src/OpenFPL_frontend/src/routes/+page.svelte
+++ src/OpenFPL_frontend/src/routes/+page.svelte
@@ -111,34 +111,24 @@
             <p class="text-gray-300 text-xxs xs:text-sm sm:text-base">
               Gameweek
             </p>
-            <p
-              class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold"
-            >
+            <p class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg xl:text-2xl mt-2 mb-2 font-bold">
               {$systemStore?.activeGameweek}
             </p>
             <p class="text-gray-300 text-xxs xs:text-sm sm:text-base">
               {$systemStore?.activeSeason.name}
             </p>
           </div>
-          <div
-            class="flex-shrink-0 w-px bg-gray-400 self-stretch"
-            style="min-width: 2px; min-height: 50px;"
-          />
+          <div class="flex-shrink-0 w-px bg-gray-400 self-stretch" style="min-width: 2px; min-height: 50px;"/>
           <div class="flex-grow">
             <p class="text-gray-300 text-xxs xs:text-sm sm:text-base">
               Managers
             </p>
-            <p
-              class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold"
-            >
+            <p class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg xl:text-2xl mt-2 mb-2 font-bold">
               {managerCount}
             </p>
             <p class="text-gray-300 text-xxs xs:text-sm sm:text-base">Total</p>
           </div>
-          <div
-            class="flex-shrink-0 w-px bg-gray-400 self-stretch"
-            style="min-width: 2px; min-height: 50px;"
-          />
+          <div class="flex-shrink-0 w-px bg-gray-400 self-stretch" style="min-width: 2px; min-height: 50px;"/>
           <div class="flex-grow">
             <p
               class="text-gray-300 hidden md:block text-xxs xs:text-sm sm:text-base"
@@ -149,7 +139,7 @@
               Weekly
             </p>
             <p
-              class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold"
+              class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg xl:text-2xl mt-2 mb-2 font-bold"
             >
               0
             </p>
@@ -164,23 +154,23 @@
         >
           <div class="flex-grow mb-3 md:mb-4 lg:mb-0">
             <p
-              class="text-gray-300 text-xxs xs:text-sm sm:text-base w-full text-center lg:w-auto lg:text-left"
+              class="text-gray-300 text-xxs xs:text-sm sm:text-base w-full text-center lg:w-auto md:text-left"
             >
               Next Game
             </p>
             <div class="flex justify-center">
               <div
-                class="flex justify-center items-center mb-2 text-xxs xs:text-sm sm:text-base"
+                class="flex justify-center items-center mb-2 lg:mb-0 text-xxs xs:text-sm sm:text-base"
               >
                 <div class="ml-4 lg:ml-1 xl:ml-4 mr-4 lg:mr-1 xl:mr-4 flex">
                   <a
-                    class="flex flex-col items-center justify-center mt-6 lg:mt-3 xl:mt-6"
+                    class="flex flex-col items-center justify-center mt-6 lg:mt-2 xl:mt-3"
                     href={`/club?id=${
                       nextFixtureHomeTeam ? nextFixtureHomeTeam.id : -1
                     }`}
                   >
                     <BadgeIcon
-                      className="h-8 lg:h-7 xl:h-8 mb-4 lg:mb-2 xl:mb-4"
+                      className="h-8 lg:h-6 xl:h-8 mb-4 lg:mb-2 xl:mb-1"
                       primaryColour={nextFixtureHomeTeam
                         ? nextFixtureHomeTeam.primaryColourHex
                         : ""}
@@ -198,20 +188,18 @@
                     </span>
                   </a>
                 </div>
-                <div
-                  class="w-v ml-1 mr-1 flex justify-center mt-6 lg:mt-2 xl:mt-6"
-                >
+                <div class="w-v ml-1 mr-1 flex justify-center mt-6 lg:mt-1 xl:mt-6">
                   <p class="text-xs mt-2 mb-2 font-bold">v</p>
                 </div>
                 <div class="ml-4 lg:ml-1 xl:ml-4 mr-4 lg:mr-1 xl:mr-4 flex">
                   <a
-                    class="flex flex-col items-center justify-center mt-6 lg:mt-3 xl:mt-6"
+                    class="flex flex-col items-center justify-center mt-6 lg:mt-2 xl:mt-3"
                     href={`/club?id=${
                       nextFixtureAwayTeam ? nextFixtureAwayTeam.id : -1
                     }`}
                   >
                     <BadgeIcon
-                      className="h-8 lg:h-7 xl:h-8 mb-4 lg:mb-2 xl:mb-4"
+                      className="h-8 lg:h-6 xl:h-8 mb-4 lg:mb-2 xl:mb-1"
                       primaryColour={nextFixtureAwayTeam
                         ? nextFixtureAwayTeam.primaryColourHex
                         : ""}
@@ -246,7 +234,7 @@
             </p>
             <div class="flex">
               <p
-                class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold"
+                class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg xl:text-2xl xl:text-2xl mt-2 mb-2 font-bold"
               >
                 {countdownDays}<span class="text-gray-300 text-xs ml-1">d</span>
                 : {countdownHours}<span class="text-gray-300 text-xs ml-1"
@@ -258,12 +246,12 @@
               </p>
             </div>
             <p
-              class="lg:hidden xl:flex text-gray-300 text-xxs xs:text-sm sm:text-base"
+              class="lg:hidden 2xl:flex text-gray-300 text-xxs xs:text-sm sm:text-base"
             >
               {nextFixtureDate} | {nextFixtureTime}
             </p>
             <p
-              class="hidden lg:flex xl:hidden text-gray-300 text-xxs xs:text-sm sm:text-base"
+              class="hidden lg:flex 2xl:hidden text-gray-300 text-xxs xs:text-sm sm:text-base"
             >
               {nextFixtureDateSmall}
             </p>
@@ -281,7 +269,7 @@
               GW {$systemStore?.focusGameweek} High Score
             </p>
             <p
-              class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold max-w-[200px] truncate"
+              class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg xl:text-2xl mt-2 mb-2 font-bold max-w-[200px] truncate"
             >
               {#if weeklyLeader}
                 <a
@@ -308,24 +296,22 @@
           class="hidden md:flex lg:hidden justify-start items-center text-white rounded-md bg-panel
           space-x-2 sm:space-x-3 md:space-x-4 flex-grow
           p-2 xs:p-3 sm:p-4
-          mb-5 xs:mb-7 sm:mb-8 lg:mb-0"
+          mb-5 xs:mb-7 sm:mb-8 md:mb-0"
         >
           <div class="flex-grow">
-            <p
-              class="text-gray-300 text-xxs xs:text-sm sm:text-base w-full text-center lg:w-auto lg:text-left"
-            >
+            <p class="text-gray-300 text-xxs xs:text-sm sm:text-base w-full text-center lg:w-auto md:text-left">
               Next Game
             </p>
             <div class="flex flex-row">
               <div class="ml-4 lg:ml-1 xl:ml-4 mr-4 lg:mr-1 xl:mr-4 flex">
                 <a
-                  class="flex flex-col items-center justify-center mt-6 lg:mt-3 xl:mt-6"
+                  class="flex flex-col items-center justify-center mt-4 lg:mt-3 xl:mt-6"
                   href={`/club?id=${
                     nextFixtureHomeTeam ? nextFixtureHomeTeam.id : -1
                   }`}
                 >
                   <BadgeIcon
-                    className="h-8 lg:h-7 xl:h-8 mb-4 lg:mb-2 xl:mb-4"
+                    className="h-8 mb-4"
                     primaryColour={nextFixtureHomeTeam
                       ? nextFixtureHomeTeam.primaryColourHex
                       : ""}
@@ -343,20 +329,18 @@
                   </span>
                 </a>
               </div>
-              <div
-                class="w-v ml-1 mr-1 flex justify-center mt-6 lg:mt-2 xl:mt-6"
-              >
+              <div class="w-v ml-1 mr-1 flex justify-center mt-6 lg:mt-2 xl:mt-6">
                 <p class="text-xs mt-2 mb-2 font-bold">v</p>
               </div>
               <div class="ml-4 lg:ml-1 xl:ml-4 mr-4 lg:mr-1 xl:mr-4 flex">
                 <a
-                  class="flex flex-col items-center justify-center mt-6 lg:mt-3 xl:mt-6"
+                  class="flex flex-col items-center justify-center mt-4 lg:mt-3 xl:mt-6"
                   href={`/club?id=${
                     nextFixtureAwayTeam ? nextFixtureAwayTeam.id : -1
                   }`}
                 >
                   <BadgeIcon
-                    className="h-8 lg:h-7 xl:h-8 mb-4 lg:mb-2 xl:mb-4"
+                    className="h-8 mb-4"
                     primaryColour={nextFixtureAwayTeam
                       ? nextFixtureAwayTeam.primaryColourHex
                       : ""}
@@ -385,7 +369,7 @@
               Kick Off
             </p>
             <p
-              class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold"
+              class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg xl:text-2xl mt-2 mb-2 font-bold"
             >
               {countdownDays}<span class="text-gray-300 text-xs ml-1">d</span>
               : {countdownHours}<span class="text-gray-300 text-xs ml-1">h</span
@@ -411,7 +395,7 @@
               GW {$systemStore?.focusGameweek} High Score
             </p>
             <p
-              class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg mt-2 mb-2 font-bold max-w-[200px] truncate"
+              class="text-xs xs:text-sm sm:text-2xl md:text-3xl lg:text-lg xl:text-2xl mt-2 mb-2 font-bold max-w-[200px] truncate"
             >
               {#if weeklyLeader}
                 <a
