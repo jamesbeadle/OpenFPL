be02a2ef244600e5ee71f452244ec92b924453e7
James Beadle <jamesbeadle1989@gmail.com>
2023-12-07 17:03:22 +0000

    Format club players list

--- src/OpenFPL_frontend/src/lib/components/team-players.svelte
+++ src/OpenFPL_frontend/src/lib/components/team-players.svelte
@@ -36,19 +36,19 @@
           </select>
         </div>
       </div>
-      <div class="flex border-b border-gray-700 bg-light-gray text-xs xs:text-sm p-2 px-4">
+      <div class="flex border-b border-gray-700 bg-light-gray text-xs xs:text-sm md:text-base p-2 xs:py-3 md:py-4 px-4">
         <div class="flex sm:hidden w-2/12">No.</div>
         <div class="hidden sm:flex w-2/12">Number</div>
         <div class="flex sm:hidden w-2/12">Pos.</div>
         <div class="hidden sm:flex w-2/12">Position</div>
-        <div class="flex w-6/12 sm:w-4/12">Player Name</div>
+        <div class="flex w-6/12 sm:w-4/12 lg:w-3/12 xl:w-3/12">Player Name</div>
+        <div class="hidden xl:flex w-1/12">Age</div>
         <div class="hidden sm:flex w-2/12">Value</div>
-        <div class="flex 2/12">&nbsp;</div>
-        <div class="hidden flex w-1/2">Age</div>
-        <div class="hidden flex w-1/2">Season Points</div>
+        <div class="hidden lg:flex w-1/12">Points</div>
+        <div class="flex 2/12 xl:w-1/12">&nbsp;</div>
       </div>
       {#each filteredPlayers as player}
-        <div class="flex items-center p-2 px-4 border-b border-gray-700 text-white cursor-pointer text-xs xs:text-sm">
+        <div class="flex items-center p-2 xs:py-3 md:py-4 px-4 border-b border-gray-700 text-white cursor-pointer text-xs xs:text-sm md:text-base">
           <a
             class="flex-grow flex items-center justify-start"
             href={`/player?id=${player.id}`}
@@ -59,27 +59,24 @@
             <div class="flex items-center w-2/12">
               {getPositionAbbreviation(player.position)}
             </div>
-            <div class="flex items-center w-6/12 sm:w-4/12">
+            <div class="flex items-center w-6/12 sm:w-4/12 lg:w-3/12 xl:w-3/12">
               <svelte:component
                 this={getFlagComponent(player.nationality)}
                 class="w-4 h-4 mr-2 hidden xs:flex"
               />
               {player.firstName} {player.lastName}
             </div>
+            <div class="hidden xl:flex items-center w-1/12">
+              {calculateAgeFromNanoseconds(Number(player.dateOfBirth))}
+            </div>
             <div class="hidden sm:flex items-center w-2/12">
               £{(Number(player.value) / 4).toFixed(2)}m
             </div>
-            <div class="flex w-2/12 justify-center">
-              <ViewDetailsIcon className='w-4' />
-            </div>
-            <div class="hidden items-center w-1/12">
-              {calculateAgeFromNanoseconds(Number(player.dateOfBirth))}
-            </div>
-            <div class="hidden items-center w-1/12">
-              
-            </div>
-            <div class="hidden items-center w-1/12">
+            <div class="hidden lg:flex items-center w-1/12">
               {player.totalPoints}
+            </div>
+            <div class="flex w-2/12 justify-center xl:justify-start xl:w-1/12">
+              <ViewDetailsIcon className='w-4 sm:w-5 md:w-6 xl:w-7' />
             </div>
           </a>
         </div>
