3a3f5f08390e603cc0d7195d22056420bc075f29
James Beadle <jamesbeadle1989@gmail.com>
2023-12-09 03:48:10 +0000

    Update manager view header

--- src/OpenFPL_frontend/src/routes/manager/+page.svelte
+++ src/OpenFPL_frontend/src/routes/manager/+page.svelte
@@ -53,6 +53,7 @@
           ? URL.createObjectURL(blob)
           : "profile_placeholder.png";
       profilePicture = blobUrl;
+      selectedSeason = $systemStore?.activeSeason.name ?? "-";
 
       const dateInMilliseconds = Number(manager.createDate / 1000000n);
       const date = new Date(dateInMilliseconds);
@@ -110,130 +111,128 @@
   {#if isLoading}
     <Spinner />
   {:else}
-    <div class="m-4">
-      <div class="flex flex-col md:flex-row">
-        <div
-          class="flex justify-start items-center text-white space-x-4 flex-grow m-4 bg-panel p-4 rounded-md"
-        >
-          <div class="flex">
-            <img class="w-20" src={profilePicture} alt={displayName} />
-          </div>
-          <div class="vertical-divider" />
-          <div class="flex-grow">
-            <p class="content-panel-header">Manager</p>
-            <p class="content-panel-stat">
-              {displayName}
-            </p>
-            <p class="content-panel-header">
-              Joined: {joinedDate}
-            </p>
-          </div>
-          <div class="vertical-divider" />
-          <div class="flex-grow">
-            <p class="content-panel-header">Favourite Team</p>
-            <p class="content-panel-stat flex items-center">
-              <BadgeIcon
-                className="w-7 mr-2"
-                primaryColour={favouriteTeam?.primaryColourHex ?? "#2CE3A6"}
-                secondaryColour={favouriteTeam?.secondaryColourHex ?? "#FFFFFF"}
-                thirdColour={favouriteTeam?.thirdColourHex ?? "#000000"}
-              />
-              {favouriteTeam?.abbreviatedName ?? "-"}
-            </p>
-            <p class="content-panel-header">
-              {favouriteTeam?.name ?? "Not Set"}
-            </p>
-          </div>
+    <div class="page-header-wrapper">
+      <div class="content-panel lg:w-1/2">
+        <div class="flex">
+          <img class="w-20" src={profilePicture} alt={displayName} />
         </div>
-        <div
-          class="flex justify-start items-center text-white space-x-4 flex-grow m-4 bg-panel p-4 rounded-md"
-        >
-          <div class="flex-grow">
-            <p class="content-panel-header">Leaderboards</p>
-            <p class="content-panel-stat">
-              {manager.weeklyPosition}
-              <span class="text-xs"
-                >({manager.weeklyPoints.toLocaleString()})</span
-              >
-            </p>
-            <p class="content-panel-header">Weekly</p>
-          </div>
-          <div class="vertical-divider" />
-          <div class="flex-grow">
-            <p class="content-panel-header">
-              {favouriteTeam?.friendlyName ?? "Not Entered"}
-            </p>
-            <p class="content-panel-stat">
-              {manager.monthlyPosition}
-              <span class="text-xs"
-                >({favouriteTeam
-                  ? manager.monthlyPoints.toLocaleString()
-                  : "-"})</span
-              >
-            </p>
-            <p class="content-panel-header">Club</p>
-          </div>
-          <div class="vertical-divider" />
-          <div class="flex-grow">
-            <p class="content-panel-header">
-              {selectedSeason}
-            </p>
-            <p class="content-panel-stat">
-              {manager.seasonPosition}
-              <span class="text-xs"
-                >({manager.seasonPoints.toLocaleString()})</span
-              >
-            </p>
-            <p class="content-panel-header">Season</p>
-          </div>
+
+        <div class="vertical-divider" />
+
+        <div class="flex-grow">
+          <p class="content-panel-header">Manager</p>
+          <p class="content-panel-stat">
+            {displayName}
+          </p>
+          <p class="content-panel-header">
+            Joined: {joinedDate}
+          </p>
+        </div>
+
+        <div class="vertical-divider" />
+
+        <div class="flex-grow">
+          <p class="content-panel-header">Favourite Team</p>
+          <p class="content-panel-stat flex items-center">
+            <BadgeIcon
+              className="w-7 mr-2"
+              primaryColour={favouriteTeam?.primaryColourHex ?? "#2CE3A6"}
+              secondaryColour={favouriteTeam?.secondaryColourHex ?? "#FFFFFF"}
+              thirdColour={favouriteTeam?.thirdColourHex ?? "#000000"}
+            />
+            {favouriteTeam?.abbreviatedName ?? "-"}
+          </p>
+          <p class="content-panel-header">
+            {favouriteTeam?.name ?? "Not Set"}
+          </p>
+        </div>
+
+      </div>
+      <div class="content-panel lg:w-1/2">
+        <div class="flex-grow">
+          <p class="content-panel-header">Leaderboards</p>
+          <p class="content-panel-stat">
+            {manager.weeklyPosition}
+            <span class="text-xs"
+              >({manager.weeklyPoints.toLocaleString()})</span
+            >
+          </p>
+          <p class="content-panel-header">Weekly</p>
+        </div>
+
+        <div class="vertical-divider" />
+        <div class="flex-grow">
+          <p class="content-panel-header">
+            {favouriteTeam?.friendlyName ?? "Not Entered"}
+          </p>
+          <p class="content-panel-stat">
+            {manager.monthlyPosition}
+            <span class="text-xs"
+              >({favouriteTeam
+                ? manager.monthlyPoints.toLocaleString()
+                : "-"})</span
+            >
+          </p>
+          <p class="content-panel-header">Club</p>
+        </div>
+        <div class="vertical-divider" />
+        <div class="flex-grow">
+          <p class="content-panel-header">
+            {selectedSeason}
+          </p>
+          <p class="content-panel-stat">
+            {manager.seasonPosition}
+            <span class="text-xs"
+              >({manager.seasonPoints.toLocaleString()})</span
+            >
+          </p>
+          <p class="content-panel-header">Season</p>
         </div>
       </div>
-      <div class="flex flex-col bg-panel m-4 rounded-md">
-        <div
-          class="flex justify-between items-center text-white px-4 pt-4 rounded-md w-full"
-        >
-          <div class="flex">
-            <button
-              class={`btn ${
-                activeTab === "details" ? `fpl-button` : `inactive-btn`
-              } px-4 py-2 rounded-l-md  min-w-[125px]`}
-              on:click={() => setActiveTab("details")}
-            >
-              Details
-            </button>
-            <button
-              class={`btn ${
-                activeTab === "gameweeks" ? `fpl-button` : `inactive-btn`
-              } px-4 py-2 rounded-r-md min-w-[125px]`}
-              on:click={() => setActiveTab("gameweeks")}
-            >
-              Gameweeks
-            </button>
-          </div>
-
-          <div class="px-4">
-            {#if $fantasyTeam && activeTab === "details"}
-              <span class="text-2xl">Total Points: {$fantasyTeam?.points}</span>
-            {/if}
-          </div>
+    </div>
+    <div class="flex flex-col bg-panel m-4 rounded-md">
+      <div class="flex justify-between items-center text-white px-4 pt-4 rounded-md w-full">
+        <div class="flex">
+          <button
+            class={`btn ${
+              activeTab === "details" ? `fpl-button` : `inactive-btn`
+            } px-4 py-2 rounded-l-md  min-w-[125px]`}
+            on:click={() => setActiveTab("details")}
+          >
+            Details
+          </button>
+          <button
+            class={`btn ${
+              activeTab === "gameweeks" ? `fpl-button` : `inactive-btn`
+            } px-4 py-2 rounded-r-md min-w-[125px]`}
+            on:click={() => setActiveTab("gameweeks")}
+          >
+            Gameweeks
+          </button>
         </div>
 
-        <div class="w-full">
-          {#if activeTab === "details"}
-            <ManagerGameweekDetails
-              loadingGameweek={loadingGameweekDetail}
-              {selectedGameweek}
-              {fantasyTeam}
-            />
-          {/if}
-          {#if activeTab === "gameweeks"}
-            <ManagerGameweeks
-              {viewGameweekDetail}
-              {selectedGameweek}
-              principalId={manager.principalId}
-            />
+        <div class="px-4">
+          {#if $fantasyTeam && activeTab === "details"}
+            <span class="text-2xl">Total Points: {$fantasyTeam?.points}</span>
           {/if}
         </div>
+      </div>
+
+      <div class="w-full">
+        {#if activeTab === "details"}
+          <ManagerGameweekDetails
+            loadingGameweek={loadingGameweekDetail}
+            {selectedGameweek}
+            {fantasyTeam}
+          />
+        {/if}
+        {#if activeTab === "gameweeks"}
+          <ManagerGameweeks
+            {viewGameweekDetail}
+            {selectedGameweek}
+            principalId={manager.principalId}
+          />
+        {/if}
       </div>
     </div>
   {/if}
