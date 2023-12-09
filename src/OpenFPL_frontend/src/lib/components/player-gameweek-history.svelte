a63171149ac1bd3bf89910d88dfcdb7157572edb
James Beadle <jamesbeadle1989@gmail.com>
2023-12-09 16:49:31 +0000

    Optimise player gw history rows

--- src/OpenFPL_frontend/src/lib/components/player-gameweek-history.svelte
+++ src/OpenFPL_frontend/src/lib/components/player-gameweek-history.svelte
@@ -133,10 +133,12 @@
       <div
         class="flex justify-between p-2 border border-gray-700 py-4 bg-light-gray"
       >
-        <div class="w-1/4 px-4">Gameweek</div>
-        <div class="w-1/4 px-4">Opponent</div>
-        <div class="w-1/4 px-4">Points</div>
-        <div class="w-1/4 px-4">&nbsp;</div>
+        <div class="w-1/4 hidden md:flex px-4">Gameweek</div>
+        <div class="w-1/6 md:hidden px-4">GW</div>
+        <div class="w-2/6 md:w-1/4 px-4">Opponent</div>
+        <div class="md:w-1/4 hidden md:flex px-4">Points</div>
+        <div class="w-1/6 md:w-1/4 md:hidden px-4">Pts</div>
+        <div class="w-2/6 md:w-1/4 px-4">&nbsp;</div>
       </div>
 
       {#each playerDetails.gameweeks as gameweek}
@@ -144,8 +146,8 @@
         <div
           class="flex items-center justify-between p-2 py-4 border-b border-gray-700 cursor-pointer"
         >
-          <div class="w-1/4 px-4">{gameweek.number}</div>
-          <div class="w-1/4 px-4">
+          <div class="w-1/6 md:w-1/4 px-4">{gameweek.number}</div>
+          <div class="w-2/6 md:w-1/4 md:w-1/6 px-4">
             <a class="flex items-center" href={`/club?id=${opponent?.id}`}>
               <BadgeIcon
                 className="w-6 mr-2"
@@ -156,8 +158,8 @@
               {opponent?.friendlyName}</a
             >
           </div>
-          <div class="w-1/4 px-4">{gameweek.points}</div>
-          <div class="w-1/4 px-4 flex items-center">
+          <div class="w-1/6 md:w-1/4 px-4">{gameweek.points}</div>
+          <div class="w-2/6 md:w-1/4 px-4 flex items-center">
             <button on:click={() => showDetailModal(gameweek, opponent)}>
               <span class="flex items-center">
                 <ViewDetailsIcon className="w-6 mr-2" />View Details
