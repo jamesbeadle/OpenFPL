5c7093b80acd5efc5f21781e02f2c4a57f9efc46
James Beadle <jamesbeadle1989@gmail.com>
2024-01-03 08:13:45 +0000

    Add modal templates

---
+++ src/OpenFPL_frontend/src/lib/components/governance/player/revalue-player-up.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { Modal } from "@dfinity/gix-components";
+
+    export let visible: boolean;
+    export let closeDetailModal: () => void;
+</script>
+
+<Modal {visible} on:nnsClose={closeDetailModal}>
+    <div class="p-4">
+        <div class="flex justify-between items-center my-2">
+        <h3 class="default-header">Revalue Player Up</h3>
+        <button class="times-button" on:click={closeDetailModal}>&times;</button>
+        </div>
+
+        <div class="flex justify-start items-center w-full">
+        <div class="ml-4">
+            <p>Test</p>
+        </div>
+        </div>
+    </div>
+</Modal>
