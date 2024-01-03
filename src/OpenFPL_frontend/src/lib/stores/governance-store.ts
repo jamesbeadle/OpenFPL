68579c5a875d5bef1baa2f6f2a0acba8943cdb73
James Beadle <jamesbeadle1989@gmail.com>
2024-01-03 10:15:08 +0000

    Populate governance store functions

--- src/OpenFPL_frontend/src/lib/stores/governance-store.ts
+++ src/OpenFPL_frontend/src/lib/stores/governance-store.ts
@@ -1,15 +1,46 @@
 import { authStore } from "$lib/stores/auth.store";
+import { isError } from "$lib/utils/Helpers";
 import type { FixtureDTO, PlayerEventData, PlayerPosition, ShirtType } from "../../../../declarations/OpenFPL_backend/OpenFPL_backend.did";
 import { ActorFactory } from "../../utils/ActorFactory";
 
 function createGovernanceStore() {
 
   async function revaluePlayerUp(playerId: number) : Promise<void>{
-    //TODO: Implement
+    try {
+      const identityActor = await ActorFactory.createIdentityActor(
+        authStore,
+        process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+      );
+  
+      let result = identityActor.adminRevaluePlayerUp(playerId); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+  
+      if(isError(result)){
+        console.error("Error submitting proposal");
+        return;
+      }
+    } catch (error) {
+      console.error("Error submitting fixture data:", error);
+      throw error;
+    }
   }
 
   async function revaluePlayerDown(playerId: number) : Promise<void>{
-    //TODO: Implement
+    try {
+      const identityActor = await ActorFactory.createIdentityActor(
+        authStore,
+        process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+      );
+  
+      let result = identityActor.adminRevaluePlayerDown(playerId); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+  
+      if(isError(result)){
+        console.error("Error submitting proposal");
+        return;
+      }
+    } catch (error) {
+      console.error("Error submitting fixture data:", error);
+      throw error;
+    }
   }
 
   async function submitFixtureData(seasonId: number, gameweek : number, fixtureId: number, playerEventData: PlayerEventData[]) : Promise<void>{
@@ -18,35 +49,112 @@ function createGovernanceStore() {
         authStore,
         process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
       );
-      //let result = await identityActor.savePlayerEvents(
-       // fixtureId,
-      //  allPlayerEvents
-      //);
-      //TODO: Add in admin fixture submission logic
+  
+      let result = identityActor.adminSubmitFixtureData(seasonId, gameweek, fixtureId, playerEventData); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+  
+      if(isError(result)){
+        console.error("Error submitting proposal");
+        return;
+      }
     } catch (error) {
       console.error("Error submitting fixture data:", error);
       throw error;
     }
   }
 
-  async function initialFixtures(seasonId: number, seasonFixtures: FixtureDTO[]){
-    //TODO: Implement
+  async function addInitialFixtures(seasonId: number, seasonFixtures: FixtureDTO[]){
+    try {
+      const identityActor = await ActorFactory.createIdentityActor(
+        authStore,
+        process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+      );
+  
+      let result = identityActor.adminAddInitialFixtures(seasonId, seasonFixtures); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+  
+      if(isError(result)){
+        console.error("Error submitting proposal");
+        return;
+      }
+    } catch (error) {
+      console.error("Error submitting fixture data:", error);
+      throw error;
+    }
   }
 
   async function rescheduleFixture(seasonId: number, fixtureId: number, updatedFixtureGameweek: number, updatedFixtureDate: number) : Promise<void>{
-    
+    try {
+      const identityActor = await ActorFactory.createIdentityActor(
+        authStore,
+        process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+      );
+  
+      let result = identityActor.adminRescheduleFixture(seasonId, fixtureId, updatedFixtureGameweek, updatedFixtureDate); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+  
+      if(isError(result)){
+        console.error("Error submitting proposal");
+        return;
+      }
+    } catch (error) {
+      console.error("Error submitting fixture data:", error);
+      throw error;
+    }
   }
 
   async function loanPlayer(playerId: number, loanClubId: number, loanEndDate: number) : Promise<void>{
-    //TODO: Implement
+    try {
+      const identityActor = await ActorFactory.createIdentityActor(
+        authStore,
+        process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+      );
+  
+      let result = identityActor.adminLoanPlayer(playerId, loanClubId, loanEndDate); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+  
+      if(isError(result)){
+        console.error("Error submitting proposal");
+        return;
+      }
+    } catch (error) {
+      console.error("Error submitting fixture data:", error);
+      throw error;
+    }
   }
 
   async function transferPlayer(playerId: number, newClubId: number) : Promise<void>{
-    //TODO: Implement
+    try {
+      const identityActor = await ActorFactory.createIdentityActor(
+        authStore,
+        process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+      );
+  
+      let result = identityActor.adminTransferPlayer(playerId, newClubId); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+  
+      if(isError(result)){
+        console.error("Error submitting proposal");
+        return;
+      }
+    } catch (error) {
+      console.error("Error submitting fixture data:", error);
+      throw error;
+    }
   }
 
   async function recallPlayer(playerId: number) : Promise<void>{
-    //TODO: Implement
+    try {
+      const identityActor = await ActorFactory.createIdentityActor(
+        authStore,
+        process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+      );
+  
+      let result = identityActor.adminRecallPlayer(playerId); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+  
+      if(isError(result)){
+        console.error("Error submitting proposal");
+        return;
+      }
+    } catch (error) {
+      console.error("Error submitting fixture data:", error);
+      throw error;
+    }
   }
 
   async function createPlayer(
@@ -58,7 +166,22 @@ function createGovernanceStore() {
     valueQuarterMillions: number,
     dateOfBirth: number,
     nationality: number) : Promise<void>{
-    //TODO: Implement
+      try {
+        const identityActor = await ActorFactory.createIdentityActor(
+          authStore,
+          process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+        );
+    
+        let result = identityActor.adminCreatePlayer(clubId, position, firstName, lastName, shirtNumber, valueQuarterMillions, dateOfBirth, nationality); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+    
+        if(isError(result)){
+          console.error("Error submitting proposal");
+          return;
+        }
+      } catch (error) {
+        console.error("Error submitting fixture data:", error);
+        throw error;
+      }
   }
 
   async function updatePlayer(
@@ -69,28 +192,103 @@ function createGovernanceStore() {
     shirtNumber: number,
     dateOfBirth: number,
     nationality: number) : Promise<void>{
-    //TODO: Implement
+      try {
+        const identityActor = await ActorFactory.createIdentityActor(
+          authStore,
+          process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+        );
+    
+        let result = identityActor.adminUpdatePlayer(playerId, position, firstName, lastName, shirtNumber, dateOfBirth, nationality); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+    
+        if(isError(result)){
+          console.error("Error submitting proposal");
+          return;
+        }
+      } catch (error) {
+        console.error("Error submitting fixture data:", error);
+        throw error;
+      }
   }
 
   async function setPlayerInjury(
     playerId: number,
     description: string,
     expectedEndDate: number){
-    //TODO: Implement
+      try {
+        const identityActor = await ActorFactory.createIdentityActor(
+          authStore,
+          process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+        );
+    
+        let result = identityActor.adminSetPlayerInjury(playerId, description, expectedEndDate); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+    
+        if(isError(result)){
+          console.error("Error submitting proposal");
+          return;
+        }
+      } catch (error) {
+        console.error("Error submitting fixture data:", error);
+        throw error;
+      }
   }
 
   async function retirePlayer( 
     playerId: number,
     retirementDate: number) : Promise<void>{
-    //TODO: Implement
+      try {
+        const identityActor = await ActorFactory.createIdentityActor(
+          authStore,
+          process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+        );
+    
+        let result = identityActor.adminRetirePlayer(playerId, retirementDate); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+    
+        if(isError(result)){
+          console.error("Error submitting proposal");
+          return;
+        }
+      } catch (error) {
+        console.error("Error submitting fixture data:", error);
+        throw error;
+      }
   }
 
   async function unretirePlayer(playerId : number) : Promise<void>{
-    //TODO: Implement
+    try {
+      const identityActor = await ActorFactory.createIdentityActor(
+        authStore,
+        process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+      );
+  
+      let result = identityActor.adminUnretirePlayer(playerId); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+  
+      if(isError(result)){
+        console.error("Error submitting proposal");
+        return;
+      }
+    } catch (error) {
+      console.error("Error submitting fixture data:", error);
+      throw error;
+    }
   }
 
   async function promoteFormerClub(clubId: number) : Promise<void>{
-    //TODO: Implement
+    try {
+      const identityActor = await ActorFactory.createIdentityActor(
+        authStore,
+        process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+      );
+  
+      let result = identityActor.adminPromoteFormerClub(clubId); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+  
+      if(isError(result)){
+        console.error("Error submitting proposal");
+        return;
+      }
+    } catch (error) {
+      console.error("Error submitting fixture data:", error);
+      throw error;
+    }
   }
 
   async function promoteNewClub(
@@ -101,7 +299,22 @@ function createGovernanceStore() {
     thirdColourHex: string,
     abbreviatedName: string,
     shirtType: ShirtType) : Promise<void>{
-    //TODO: Implement
+      try {
+        const identityActor = await ActorFactory.createIdentityActor(
+          authStore,
+          process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+        );
+    
+        let result = identityActor.adminPromoteNewClub(name, friendlyName, primaryColourHex, secondaryColourHex, thirdColourHex, abbreviatedName, shirtType); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+    
+        if(isError(result)){
+          console.error("Error submitting proposal");
+          return;
+        }
+      } catch (error) {
+        console.error("Error submitting fixture data:", error);
+        throw error;
+      }
   }
 
   async function updateClub(
@@ -113,14 +326,29 @@ function createGovernanceStore() {
     thirdColourHex: Text,
     abbreviatedName: Text,
     shirtType: ShirtType) : Promise<void>{
-    //TODO: Implement
+      try {
+        const identityActor = await ActorFactory.createIdentityActor(
+          authStore,
+          process.env.OPENFPL_BACKEND_CANISTER_ID ?? ""
+        );
+    
+        let result = identityActor.adminUpdateClub(clubId, name, friendlyName, primaryColourHex, secondaryColourHex, thirdColourHex, abbreviatedName, shirtType); //TODO: POST SNS REPLACE WITH GOVERNANCE CANISTER CALL
+    
+        if(isError(result)){
+          console.error("Error submitting proposal");
+          return;
+        }
+      } catch (error) {
+        console.error("Error submitting fixture data:", error);
+        throw error;
+      }
   }
 
   return {
     revaluePlayerUp,
     revaluePlayerDown,
     submitFixtureData,
-    initialFixtures,
+    addInitialFixtures,
     rescheduleFixture,
     loanPlayer,
     transferPlayer,
