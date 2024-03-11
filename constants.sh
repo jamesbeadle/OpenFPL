88d405788df0911ef5f7bc4a02db740285c5693d
jamesbeadle <jamesbeadle1989@gmail.com>
2024-03-11 09:37:26 +0000

    Deploy SNS WIP

---
+++ constants.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
+
+# put your dfx identity here
+export DX_IDENT="default"
+
+# if you don't export CANISTER_TEST or set its value to "_test",
+# then the test flag is set for NNS and SNS governance canisters;
+# if you export CANISTER_TEST to be the empty string "",
+# then the test flag is not set.
+# export CANISTER_TEST=""
+
+# you can find available II releases here:
+# https://github.com/dfinity/internet-identity/tags
+export II_RELEASE="release-2023-10-30"
+
+# you can find NNS proposals upgrading system canisters here:
+# https://dashboard.internetcomputer.org/governance?topic=TOPIC_NETWORK_CANISTER_MANAGEMENT
+# NNS proposals to upgrade NNS frontend dapp are called "Upgrade Nns Canister: qoctq-giaaa-aaaaa-aaaea-cai"
+export NNS_DAPP_RELEASE="nightly-2023-10-30"
+
+# only edit IC_COMMIT to a commit to master with disk image obtained via:
+# $ ./gitlab-ci/src/artifacts/newest_sha_with_disk_image.sh origin/master
+# from the IC monorepo: https://github.com/dfinity/ic
+# if you change IC_COMMIT, then you need to rerun `source install.sh`
+export IC_COMMIT="d1315a1b9b002f177526faa5ee6d7e1e1aa3a22a"
+
+export TESTNET="local"
+
---
+++ propose_sns.sh
@@ -0,0 +1,30 @@
+set -euo pipefail
+
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
+
+export SNS_CONFIGURATION_FILE_PATH="${1:-sns_init.yaml}"
+
+. ./constants.sh normal
+
+export CURRENT_DX_IDENT="$(dfx identity whoami)"
+
+dfx identity use ic_admin
+
+sns propose \
+    --network "${NETWORK}" \
+    --test-neuron-proposer \
+    "${SNS_CONFIGURATION_FILE_PATH}"
+
+dfx canister --network "${NETWORK}" \
+    call nns-sns-wasm list_deployed_snses '(record {})' \
+    | idl2json \
+    > sns-wasm-list_deployed_snses-response.json
+NUM_SNS_INSTANCES=$(jq '.instances | length' sns-wasm-list_deployed_snses-response.json)
+
+# Need to flatten the JSON to make it compatible with quill
+jq '.instances[-1] | .[] |= .[0]' \
+    sns-wasm-list_deployed_snses-response.json \
+        > sns_canister_ids.json
+
+# Switch back to the previous identity
+dfx identity use "${CURRENT_DX_IDENT}"
