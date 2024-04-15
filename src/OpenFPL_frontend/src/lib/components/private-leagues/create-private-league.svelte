43da35aed07295923a62bc5ce260b39387f1e826
jamesbeadle <jamesbeadle1989@gmail.com>
2024-04-15 09:26:07 +0100

    Add private league views

---
+++ src/OpenFPL_frontend/src/lib/components/private-leagues/create-private-league.svelte
@@ -0,0 +1,7 @@
+
+
+<!-- League Details -->
+<!-- Entry Requirement -->
+<!-- Prize Setup -->
+<!-- Invite Users -->
+<!-- Agree terms -->
---
+++ src/OpenFPL_frontend/src/routes/league/+page.svelte
@@ -0,0 +1,3 @@
+<!-- Details -->
+<!-- Leaderboard -->
+<!-- Members -->
