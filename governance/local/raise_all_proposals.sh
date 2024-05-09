bf149acaac84561d3e7d02375dec168b8b4bc9b7
jamesbeadle <jamesbeadle1989@gmail.com>
2024-05-09 23:13:11 +0100

    Update readme

--- governance/local/raise_all_proposals.sh
+++ governance/local/raise_all_proposals.sh
@@ -2,14 +2,14 @@
 #Raise all live proposals
 
 #Add all generic functions
-../proposals/1-21.register_generic_functions.sh
+./governance/proposals/1-21.register_generic_functions.sh
 
 #Register neuron controller canister
-../proposals/register_canister_with_sns.sh br5f7-7uaaa-aaaaa-qaaca-cai "Register neuron_controller as an SNS controlled canister." "https://github.com/jamesbeadle/OpenFPL/blob/master/src/neuron_controller/actor.mo" "This canister is responsible for controlling the DAO's first neuron."
+./governance/proposals/register_canister_with_sns.sh br5f7-7uaaa-aaaaa-qaaca-cai "Register neuron_controller as an SNS controlled canister." "https://github.com/jamesbeadle/OpenFPL/blob/master/src/neuron_controller/actor.mo" "This canister is responsible for controlling the DAO's first neuron."
 
 #Transfer treasury funds x 2
-../proposals/26.ICPSwapTransferICP.sh
-../proposals/26.ICPSwapTransferFPL.sh
+./governance/proposals/26.ICPSwapTransferICP.sh
+./governance/proposals/26.ICPSwapTransferFPL.sh
 
 #Add New token generic callback function
-../proposals/31.RegisterNewToken.sh
+./governance/proposals/31.RegisterNewToken.sh
