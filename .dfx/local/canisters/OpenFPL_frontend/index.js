4860135034aa3051a34c88d627ca658b0c92775c
thillyt <73003317+thillyt@users.noreply.github.com>
2024-09-25 13:08:10 +0100

    new file:   .dfx/local/canister_ids.json 	modified:   .dfx/local/canisters/OpenFPL_backend/OpenFPL_backend.wasm 	modified:   .dfx/local/canisters/OpenFPL_backend/index.js 	modified:   .dfx/local/canisters/OpenFPL_backend/service.did.d.ts 	modified:   .dfx/local/canisters/OpenFPL_backend/service.did.js 	new file:   .dfx/local/canisters/OpenFPL_frontend/OpenFPL_frontend.wasm.gz 	modified:   .dfx/local/canisters/OpenFPL_frontend/assetstorage.did 	new file:   .dfx/local/canisters/OpenFPL_frontend/assetstorage.wasm.gz 	new file:   .dfx/local/canisters/OpenFPL_frontend/constructor.did 	new file:   .dfx/local/canisters/OpenFPL_frontend/index.js 	new file:   .dfx/local/canisters/OpenFPL_frontend/init_args.txt 	new file:   .dfx/local/canisters/OpenFPL_frontend/service.did 	new file:   .dfx/local/canisters/OpenFPL_frontend/service.did.d.ts 	new file:   .dfx/local/canisters/OpenFPL_frontend/service.did.js 	deleted:    .dfx/local/canisters/idl/pjm5n-dy5gh-ocojj-63k3a.did 	deleted:    .dfx/local/canisters/idl/uev7d-77njl-43bvd-lsbla.did 	deleted:    .dfx/local/canisters/idl/wdah5-aivl6-el7fc-tzvaa.did 	deleted:    .dfx/local/canisters/idl/yyxth-iooen-jn3m6-6zehq.did 	modified:   .dfx/local/canisters/neuron_controller/index.js 	modified:   .dfx/local/canisters/neuron_controller/neuron_controller.wasm 	modified:   .dfx/local/canisters/neuron_controller/service.did.d.ts 	modified:   .dfx/local/canisters/neuron_controller/service.did.js 	renamed:    .dfx/local/lsp/eo24i-x6y7x-3kosc-ia7oq.did -> .dfx/local/lsp/aax3a-h4aaa-aaaaa-qaahq-cai.did 	renamed:    .dfx/local/lsp/22i5y-limfo-yznx2-w4aba.did -> .dfx/local/lsp/c2lt4-zmaaa-aaaaa-qaaiq-cai.did 	new file:   .dfx/local/lsp/c5kvi-uuaaa-aaaaa-qaaia-cai.did 	deleted:    .dfx/local/lsp/fikb6-mzmcp-lwvnt-ewdoq.did 	deleted:    .dfx/local/lsp/iclh7-gxafo-5buo5-kfnga.did 	deleted:    .dfx/local/lsp/jfefj-6vmmq-nqrgy-r4vra.did 	deleted:    .dfx/local/lsp/jgljh-gpzqa-rtlki-5qtqq.did 	deleted:    .dfx/local/lsp/kxhur-gdnxf-flvkr-6rmba.did 	deleted:    .dfx/local/lsp/l6ahz-ybkka-mue7t-jhidq.did 	deleted:    .dfx/local/lsp/mcy37-gqkpc-ra5jc-eaxja.did 	deleted:    .dfx/local/lsp/x53ie-v2t6w-fwil2-pw4ca.did 	modified:   .dfx/local/network-id 	modified:   .env 	modified:   .svelte-kit/ambient.d.ts 	modified:   .svelte-kit/generated/server/internal.js 	modified:   .svelte-kit/output/server/chunks/index.js 	modified:   build/index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/OpenFPL_frontend/src/app.html

--- .dfx/local/canisters/OpenFPL_frontend/index.js
+++ .dfx/local/canisters/OpenFPL_frontend/index.js
@@ -1,17 +1,17 @@
 import { Actor, HttpAgent } from "@dfinity/agent";
 
 // Imports and re-exports candid interface
-import { idlFactory } from './OpenFPL_backend.did.js';
-export { idlFactory } from './OpenFPL_backend.did.js';
+import { idlFactory } from './OpenFPL_frontend.did.js';
+export { idlFactory } from './OpenFPL_frontend.did.js';
 // CANISTER_ID is replaced by webpack based on node environment
-export const canisterId = process.env.CANISTER_ID_OPENFPL_BACKEND;
+export const canisterId = process.env.CANISTER_ID_OPENFPL_FRONTEND;
 
 /**
  * @deprecated since dfx 0.11.1
  * Do not import from `.dfx`, instead switch to using `dfx generate` to generate your JS interface.
  * @param {string | import("@dfinity/principal").Principal} canisterId Canister ID of Agent
  * @param {{agentOptions?: import("@dfinity/agent").HttpAgentOptions; actorOptions?: import("@dfinity/agent").ActorConfig} | { agent?: import("@dfinity/agent").Agent; actorOptions?: import("@dfinity/agent").ActorConfig }} [options]
- * @return {import("@dfinity/agent").ActorSubclass<import("./OpenFPL_backend.did.js")._SERVICE>}
+ * @return {import("@dfinity/agent").ActorSubclass<import("./OpenFPL_frontend.did.js")._SERVICE>}
  */
 export const createActor = (canisterId, options = {}) => {
   console.warn(`Deprecation warning: you are currently importing code from .dfx. Going forward, refactor to use the dfx generate command for JavaScript bindings.
@@ -36,7 +36,7 @@ See https://internetcomputer.org/docs/current/developer-docs/updates/release-not
 };
   
 /**
- * A ready-to-use agent for the OpenFPL_backend canister
- * @type {import("@dfinity/agent").ActorSubclass<import("./OpenFPL_backend.did.js")._SERVICE>}
+ * A ready-to-use agent for the OpenFPL_frontend canister
+ * @type {import("@dfinity/agent").ActorSubclass<import("./OpenFPL_frontend.did.js")._SERVICE>}
  */
-export const OpenFPL_backend = createActor(canisterId);
+export const OpenFPL_frontend = createActor(canisterId);
