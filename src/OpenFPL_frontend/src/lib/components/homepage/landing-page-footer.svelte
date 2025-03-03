d867379d450a1927ba5b97048f139e9181e559bf
thillyt <73003317+thillyt@users.noreply.github.com>
2025-03-03 13:20:55 +0000

    Mobile Fix

--- src/OpenFPL_frontend/src/lib/components/homepage/landing-page-footer.svelte
+++ src/OpenFPL_frontend/src/lib/components/homepage/landing-page-footer.svelte
@@ -5,7 +5,6 @@
     
     <div class="flex items-center justify-between w-full p-4">
         <div class="flex items-center space-x-2">
-         
             <a
             href="https://oc.app/community/uf3iv-naaaa-aaaar-ar3ta-cai/?ref=zv6hh-xaaaa-aaaar-ac35q-cai"
             target="_blank"
@@ -31,28 +30,20 @@
             >
             <img src="/github.png" class="w-auto h-4 mb-2" alt="GitHub" />
             </a>
-
-
-
-
         </div>
       
         <div class="flex-1 mx-4 text-center">
         </div>
       
         <div class="flex items-center space-x-2">
-         
             <a
-        href="https://juno.build"
-        target="_blank"
-        class="flex items-center hover:text-gray-300"
-        >
-        Sponsored By juno.build
-        <JunoIcon className="h-8 w-auto ml-2" />
+                href="https://juno.build"
+                target="_blank"
+                class="flex items-center hover:text-gray-300"
+            >
+            Sponsored By juno.build
+            <JunoIcon className="h-8 w-auto ml-2" />
         </a>
-
-
         </div>
-    </div>
-    
+    </div>   
 </footer>
--- src/OpenFPL_frontend/src/lib/components/homepage/landing-page.svelte
+++ src/OpenFPL_frontend/src/lib/components/homepage/landing-page.svelte
@@ -14,17 +14,17 @@
 </script>
 
 <div class="flex flex-col w-full min-h-screen space-y-4 md:bg-BrandGrayBg md:p-3 md:flex-row">
-    <div class="relative flex flex-col w-full min-h-screen bg-center bg-cover md:rounded-lg md:w-1/2" style="background-image: url('landing-background.jpeg')">
+    <div class="relative flex flex-col w-full h-full bg-center bg-cover md:rounded-lg md:w-1/2" style="background-image: url('landing-background.jpeg')">
         <div class="items-end h-full p-4 md:flex bg-gradient-to-t from-black/70 to-transparent md:rounded-lg">
             <h1 class="hidden w-full text-2xl font-bold text-center md:block md:mb-8 md:py-1">
                 The world's game on the world's computer
             </h1>
-            <div class="py-24 mx-4 rounded-lg bg-BrandGrayBg md:hidden">
+            <div class="flex items-center justify-center min-h-[55%] mx-4 my-24 rounded-lg bg-BrandGray md:hidden">
                 <LandingPageDisplay {handleLoginClick} />
             </div>
     
             <div class="pt-4 pb-6 md:hidden">
-                <h2 class="mx-4 text-xl text-center xxs:pb-8 text-bold md:hidden">
+                <h2 class="mx-4 text-2xl text-center xxs:pb-8 text-bold md:hidden">
                     The world's game on the world's computer
                 </h2>
                 <div class="md:hidden">
