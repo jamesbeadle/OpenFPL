a70075dacf4c21d3d1da6e9725b5545f9e2cf668
thillyt <73003317+thillyt@users.noreply.github.com>
2025-03-03 13:21:57 +0000

    Update landing-page-footer.svelte

--- src/OpenFPL_frontend/src/lib/components/homepage/landing-page-footer.svelte
+++ src/OpenFPL_frontend/src/lib/components/homepage/landing-page-footer.svelte
@@ -1,42 +1,49 @@
 <script lang="ts">
     import JunoIcon from "$lib/icons/JunoIcon.svelte";
 </script>
-<footer class="absolute flex flex-row items-center justify-between w-full bottom-2 md:w-1/2">
-    <div class="flex flex-row space-x-2 md:px-2 md:w-1/2">
-        <a
-        href="https://oc.app/community/uf3iv-naaaa-aaaar-ar3ta-cai/?ref=zv6hh-xaaaa-aaaar-ac35q-cai"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-        <img
-            src="/openchat.png"
-            class="w-auto h-4 mb-2 mr-2"
-            alt="OpenChat"
-        />
-        </a>
-        <a
-        href="https://x.com/OpenFPL_DAO"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-        <img src="/twitter.png" class="w-auto h-4 mb-2 mr-2" alt="X" />
-        </a>
-        <a
-        href="https://github.com/jamesbeadle/OpenFPL"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-        <img src="/github.png" class="w-auto h-4 mb-2" alt="GitHub" />
-        </a>
-    </div>
-        <div class="px-4 text-xs text-right md:px-auto">
+<footer class="absolute bottom-0 flex flex-row w-full md:w-1/2">
+    
+    <div class="flex items-center justify-between w-full p-4">
+        <div class="flex items-center space-x-2">
             <a
-            href="https://juno.build"
+            href="https://oc.app/community/uf3iv-naaaa-aaaar-ar3ta-cai/?ref=zv6hh-xaaaa-aaaar-ac35q-cai"
             target="_blank"
-            class="flex items-center hover:text-gray-300"
+            rel="noopener noreferrer"
+            >
+            <img
+                src="/openchat.png"
+                class="w-auto h-4 mb-2 mr-2"
+                alt="OpenChat"
+            />
+            </a>
+            <a
+            href="https://x.com/OpenFPL_DAO"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <img src="/twitter.png" class="w-auto h-4 mb-2 mr-2" alt="X" />
+            </a>
+            <a
+            href="https://github.com/jamesbeadle/OpenFPL"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <img src="/github.png" class="w-auto h-4 mb-2" alt="GitHub" />
+            </a>
+        </div>
+      
+        <div class="flex-1 mx-4 text-center">
+        </div>
+      
+        <div class="flex items-center space-x-2">
+            <a
+                href="https://juno.build"
+                target="_blank"
+                class="flex items-center hover:text-gray-300"
             >
             Sponsored By juno.build
             <JunoIcon className="h-8 w-auto ml-2" />
-            </a>
+        </a>
         </div>
+    </div>   
 </footer>
