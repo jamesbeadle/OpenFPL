005f2327e5af2bb4777651a5ab28607518d9f47a
jamesbeadle <jamesbeadle1989@gmail.com>
2025-03-27 11:54:15 +0000

    Update layout

---
+++ src/OpenFPL_frontend/src/lib/components/profile/invalid-membership-page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import CopyPrincipal from "./copy-principal.svelte";
+</script>
+
+
+<div class="flex flex-col space-y-4">
+    <p>Only ICFC ecosystem owners can play OpenFPL.</p>
+    <p>Please visit <a href="https://icfc.app">https://icfc.app</a> to setup your membership and link your OpenFPL principal ID:</p>
+    <CopyPrincipal />
+    <button class="brand-button">Refresh</button>
+</div>
--- src/OpenFPL_frontend/src/lib/services/user-service.ts
+++ src/OpenFPL_frontend/src/lib/services/user-service.ts
@@ -1,6 +1,9 @@
 import { ActorFactory } from "../utils/actor.factory";
 import { isError } from "../utils/helpers";
-import type { ProfileDTO } from "../../../../declarations/OpenFPL_backend/OpenFPL_backend.did";
+import type {
+  ICFCMembershipDTO,
+  ProfileDTO,
+} from "../../../../declarations/OpenFPL_backend/OpenFPL_backend.did";
 import { authStore } from "$lib/stores/auth.store";
 import { toasts } from "$lib/stores/toasts-store";
 
@@ -24,5 +27,13 @@ export class UserService {
       });
       throw error;
     }
+  }
+
+  async getUserIFCFMembership(): Promise<ICFCMembershipDTO | undefined> {
+    const identityActor: any = await ActorFactory.createIdentityActor(
+      authStore,
+      process.env.OPENFPL_BACKEND_CANISTER_ID ?? "",
+    );
+    return await identityActor.getUserIFCFMembership();
   }
 }
