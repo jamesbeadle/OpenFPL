d34ff7a76a6114ccaf79783290afad1641c8d3f8
jamesbeadle <jamesbeadle1989@gmail.com>
2025-04-25 10:54:04 +0100

    Add high score views

--- src/OpenFPL_frontend/src/lib/components/home/trophy-room.svelte
+++ src/OpenFPL_frontend/src/lib/components/home/trophy-room.svelte
@@ -1,27 +1,15 @@
-<!--
-
-    // TODO
-    
-    All Time high scores
-        - Show record value
-        - Show record held by
-        - Show value of pot if broken
-
-    For
-    - Weekly high score
-    - Monthly high score
-    - Season high score
-
--->
-
 <script lang="ts">
     import { onMount } from "svelte";
     import LocalSpinner from "../shared/global/local-spinner.svelte";
+    import type { AllTimeHighScores } from "../../../../../declarations/OpenFPL_backend/OpenFPL_backend.did";
+    import { allTimeHighScoresStore } from "$lib/stores/all-time-high-score-store";
     
     let isLoading = $state(true);
+    let allTimeHighScores: AllTimeHighScores | undefined = $state(undefined);
     
     onMount(async () => {
         try {
+            allTimeHighScores = await allTimeHighScoresStore.getAllTimeHighScores({});
         } catch (error) {
         } finally {
             isLoading = false;
@@ -35,32 +23,30 @@
 {#if isLoading}
       <LocalSpinner />
 {:else}
-  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 min-h-64">
-      <div class="bg-BrandGray text-left rounded shadow-md p-4 hover:shadow-lg transition-shadow flex items-center flex-col">
-        
-        <h3 class="w-full text-2xl font-semibold">All Time Weekly High Score</h3>
-        <p class="w-full text-sm font-bold text-BrandLightGray">{weeklyLeaderboardATHScore.currentRecord}</p>
-        <p>Next Winner Prize Pool: {weeklyLeaderboardATHScore.currentPrizePool}</p>
-        <p>Record Holder: {weeklyLeaderboardATHScore.recordHolderUsername}</p>
-        
-    </div>
-    </div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 min-h-64">
-    <div class="bg-BrandGray text-left rounded shadow-md p-4 hover:shadow-lg transition-shadow flex items-center flex-col">
-      
-      <h3 class="w-full text-2xl font-semibold">All Time Monthly High Score</h3>
-      <p class="w-full text-sm font-bold text-BrandLightGray">{monthlyLeaderboardATHScore.currentRecord}</p>
-      <p>Next Winner Prize Pool: {monthlyLeaderboardATHScore.currentPrizePool}</p>
-      <p>Record Holder: {monthlyLeaderboardATHScore.recordHolderUsername}</p>
-     </div>
-    </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 min-h-64">
         <div class="bg-BrandGray text-left rounded shadow-md p-4 hover:shadow-lg transition-shadow flex items-center flex-col">
         
-        <h3 class="w-full text-2xl font-semibold">All Time Season High Score</h3>
-        <p class="w-full text-sm font-bold text-BrandLightGray">{seasonLeaderboardATHScore.currentRecord}</p>
-        <p>Next Winner Prize Pool: {seasonLeaderboardATHScore.currentPrizePool}</p>
-        <p>Record Holder: {seasonLeaderboardATHScore.recordHolderUsername}</p>
-    </div>
+            <h3 class="w-full text-2xl font-semibold">All Time Weekly High Score</h3>
+            <p class="w-full text-sm font-bold text-BrandLightGray">{allTimeHighScores?.weeklyHighScore[0]?.recordPoints}</p>
+            <p>Next Winner Prize Pool: {allTimeHighScores?.weeklyHighScore[0]?.recordPrizePool}</p>
+            <p>Record Holder: {allTimeHighScores?.weeklyHighScore[0]?.recordHolderUsername}</p>
+        
+        </div>
+        <div class="bg-BrandGray text-left rounded shadow-md p-4 hover:shadow-lg transition-shadow flex items-center flex-col">
+        
+            <h3 class="w-full text-2xl font-semibold">All Time Monthly High Score</h3>
+            <p class="w-full text-sm font-bold text-BrandLightGray">{allTimeHighScores?.monthlyHighScore[0]?.recordPoints}</p>
+            <p>Next Winner Prize Pool: {allTimeHighScores?.monthlyHighScore[0]?.recordPrizePool}</p>
+            <p>Record Holder: {allTimeHighScores?.monthlyHighScore[0]?.recordHolderUsername}</p>
+        
+        </div>
+        <div class="bg-BrandGray text-left rounded shadow-md p-4 hover:shadow-lg transition-shadow flex items-center flex-col">
+        
+            <h3 class="w-full text-2xl font-semibold">All Time Season High Score</h3>
+            <p class="w-full text-sm font-bold text-BrandLightGray">{allTimeHighScores?.seasonHighScore[0]?.recordPoints}</p>
+            <p>Next Winner Prize Pool: {allTimeHighScores?.seasonHighScore[0]?.recordPrizePool}</p>
+            <p>Record Holder: {allTimeHighScores?.seasonHighScore[0]?.recordHolderUsername}</p>
+        
+        </div>
     </div>
 {/if}
