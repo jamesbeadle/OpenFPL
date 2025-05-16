57f0f3cfc7f417879326011ba0996f05159af424
jamesbeadle <jamesbeadle1989@gmail.com>
2025-05-16 19:59:42 +0100

    add attributes

---
+++ src/OpenFPL_frontend/src/lib/components/player/player-detail-row.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import type { Player } from "../../../../../declarations/OpenFPL_backend/OpenFPL_backend.did";
+
+    interface Props {
+        player: Player;
+    }
+
+    let { player }: Props = $props();
+</script>
+<p>{player.firstName} {player.lastName}</p>
